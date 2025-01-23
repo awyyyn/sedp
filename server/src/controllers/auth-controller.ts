5985972b6fa3eca764142f8cedf33f1ca8b65d51
awynx <alwin.puche16@gmail.com>
2025-01-23 13:14:08 +0800

    feat: implement authentication routes and controllers; update Prisma schema for user management

---
+++ server/src/controllers/auth-controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const loginController = async (req: Request, res: Response) => {};
+export const registerController = async (req: Request, res: Response) => {};
+export const forgotPasswordController = async (
+	req: Request,
+	res: Response
+) => {};
---
+++ server/src/routes/auth-routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {
+	loginController,
+	registerController,
+} from "../controllers/auth-controller.js";
+
+const router = Router();
+
+router.post("/login", loginController);
+router.post("/register", registerController);
+
+export { router as authRoutes };
