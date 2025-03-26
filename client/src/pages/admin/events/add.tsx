11ca93eaae1cfa3429298091829e73a2aa6c8901
awynx <alwin.puche16@gmail.com>
2025-03-26 22:55:05 +0800

    Add event management pages: AddEvent, EditEvent, EventInfo, and EventList components

---
+++ client/src/pages/admin/events/add.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import EventForm from "./__components/form";
+
+export default function AddEvent() {
+	return (
+		<div>
+			<EventForm />
+		</div>
+	);
+}
---
+++ client/src/pages/admin/events/edit.tsx
@@ -0,0 +1,9 @@
+import EventForm from "./__components/form";
+
+export default function EditEvent() {
+	return (
+		<div>
+			<EventForm />
+		</div>
+	);
+}
