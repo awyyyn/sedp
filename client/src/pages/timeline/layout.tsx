0f1c129e8d624ea34ace4a51e60ec488dd1a636b
awynx <alwin.puche16@gmail.com>
2025-03-31 08:14:33 +0800

    Add user, account, and timeline layouts: implement responsive layouts with navigation buttons and outlet rendering for user, account, and timeline pages.

---
+++ client/src/pages/timeline/layout.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@heroui/button";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+export default function TimelineLayout() {
+	const { pathname } = useLocation();
+	const links = [
+		"/timeline/events",
+		"/timeline/meetings",
+		"/timeline/announcements",
+	];
+
+	return (
+		<div className="max-h-screen pt-24 overflow-hidden">
+			<div className="flex gap-2 pb-2 px-5 md:px-0 container mx-auto    ">
+				{links.map((link) => (
+					<Button
+						key={link}
+						as={Link}
+						to={link}
+						color={pathname === link ? "primary" : "default"}
+						radius="sm"
+						className="border-none capitalize"
+						variant={pathname === link ? "solid" : "ghost"}>
+						{link.split("/")[link.split("/").length - 1]}
+					</Button>
+				))}
+			</div>
+			<div className="overflow-y-auto pt-5 pb-10 h-[calc(100dvh-16dvh)]  ">
+				<Outlet />
+			</div>
+		</div>
+	);
+}
