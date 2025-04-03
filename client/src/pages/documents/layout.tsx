1c6603f9b31393990a7ced590db5e35431627c22
awynx <alwin.puche16@gmail.com>
2025-04-03 21:05:04 +0800

    Add icons to document navigation buttons; conditionally render based on path

--- client/src/pages/documents/layout.tsx
+++ client/src/pages/documents/layout.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@heroui/button";
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { Link, Outlet, useLocation } from "react-router-dom";
 
 export default function DocumentsLayout() {
@@ -7,20 +8,31 @@ export default function DocumentsLayout() {
 
 	return (
 		<div className="max-h-screen pt-24 overflow-hidden">
-			<div className="flex gap-2 pb-2 px-5 md:px-0 container mx-auto    ">
-				{links.map((link) => (
-					<Button
-						key={link}
-						as={Link}
-						to={link}
-						color={pathname.includes(link) ? "primary" : "default"}
-						radius="sm"
-						className="border-none capitalize"
-						variant={pathname.includes(link) ? "solid" : "ghost"}>
-						{link.split("/")[link.split("/").length - 1].split("-").join(" ")}
-					</Button>
-				))}
-			</div>
+			{pathname !== "/my-documents" && (
+				<div className="flex gap-2 pb-2 px-5 md:px-0 container mx-auto    ">
+					{links.map((link, index) => (
+						<Button
+							key={link}
+							as={Link}
+							to={link}
+							color={pathname.includes(link) ? "primary" : "default"}
+							radius="sm"
+							className="border-none capitalize"
+							variant={pathname.includes(link) ? "solid" : "ghost"}>
+							{index ? (
+								<Icon icon="noto-v1:books" width="20" height="20" />
+							) : (
+								<Icon
+									icon="fluent-color:calendar-clock-24"
+									width="20"
+									height="20"
+								/>
+							)}
+							{link.split("/")[link.split("/").length - 1].split("-").join(" ")}
+						</Button>
+					))}
+				</div>
+			)}
 			<div className="overflow-y-auto pt-5 pb-10 h-[calc(100dvh-16dvh)]  ">
 				<Outlet />
 			</div>
