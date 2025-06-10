5465060b113519e1785883f8c7bb548d664c3b79
awynx <alwin.puche16@gmail.com>
2025-06-10 10:04:03 +0800

    Add Helmet for SEO and enhance registration form structure

--- client/src/pages/auth/student/forgot-password.tsx
+++ client/src/pages/auth/student/forgot-password.tsx
@@ -5,6 +5,7 @@ import { Icon } from "@iconify/react";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
 import { InputOtp } from "@heroui/input-otp";
+import { Helmet } from "react-helmet";
 
 export default function StudentForgotPassword() {
 	const navigate = useNavigate();
@@ -163,222 +164,237 @@ export default function StudentForgotPassword() {
 	const handleShowPassword = () => setShowPassword((showPass) => !showPass);
 
 	return (
-		<div className="  w-[95%] p-1 sm:w-[70%] md:w-[50%] lg:max-w-[450px]">
-			<div className="space-y-2">
-				<div className="space-y-5 flex flex-col justify-start md:justify-center items-start md:items-center">
-					{
-						[
-							<Icon
-								icon="ic:round-fingerprint"
-								key="fingerprint"
-								className="scale-125"
-								height={40}
-								width={40}
-							/>,
-							<Icon
-								icon="hugeicons:mail-open"
-								key="mail-open"
-								className="scale-125"
-								height={40}
-								width={40}
-							/>,
-							<Icon
-								icon="fluent:password-24-filled"
-								key="password"
-								className="scale-125"
-								height={40}
-								width={40}
-							/>,
-							<Icon
-								icon="solar:verified-check-bold"
-								key="verified"
-								className="scale-125"
-								height={40}
-								width={40}
-							/>,
-						][step - 1]
-					}
-					<div>
-						<h5 className="text-2xl md:text-center">
-							{
-								[
-									"Forgot Password?",
-									"Password Reset",
-									"Set new Password",
-									"All Done!",
-								][step - 1]
-							}
-						</h5>
-						<p className="text-gray-500 text-start md:text-center lg:max-w-[70%] lg:mx-auto">
-							{
-								[
-									"No worries, we'll send you an email to reset your password.",
-									`We sent a code to ${values.email}. Please check your email.`,
-									"Must be at least 8 characters.",
-									`Your password has been reset! You can now login with your new password.`,
-								][step - 1]
-							}
-						</p>
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Forgot Password | SEDP</title>
+				<meta
+					name="description"
+					content="Forgot your password? No worries, we'll send you an email to reset your password."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Helmet>
+			<div className="  w-[95%] p-1 sm:w-[70%] md:w-[50%] lg:max-w-[450px]">
+				<div className="space-y-2">
+					<div className="space-y-5 flex flex-col justify-start md:justify-center items-start md:items-center">
+						{
+							[
+								<Icon
+									icon="ic:round-fingerprint"
+									key="fingerprint"
+									className="scale-125"
+									height={40}
+									width={40}
+								/>,
+								<Icon
+									icon="hugeicons:mail-open"
+									key="mail-open"
+									className="scale-125"
+									height={40}
+									width={40}
+								/>,
+								<Icon
+									icon="fluent:password-24-filled"
+									key="password"
+									className="scale-125"
+									height={40}
+									width={40}
+								/>,
+								<Icon
+									icon="solar:verified-check-bold"
+									key="verified"
+									className="scale-125"
+									height={40}
+									width={40}
+								/>,
+							][step - 1]
+						}
+						<div>
+							<h5 className="text-2xl md:text-center">
+								{
+									[
+										"Forgot Password?",
+										"Password Reset",
+										"Set new Password",
+										"All Done!",
+									][step - 1]
+								}
+							</h5>
+							<p className="text-gray-500 text-start md:text-center lg:max-w-[70%] lg:mx-auto">
+								{
+									[
+										"No worries, we'll send you an email to reset your password.",
+										`We sent a code to ${values.email}. Please check your email.`,
+										"Must be at least 8 characters.",
+										`Your password has been reset! You can now login with your new password.`,
+									][step - 1]
+								}
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="space-y-5 md:mt-5 ">
-				{
-					[
-						<Input
-							key="email"
-							name="email"
-							label="Email"
-							fullWidth
-							value={values.email}
-							isReadOnly={loading}
-							onValueChange={(value) => {
-								const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-								if (!emailRegex.test(value)) {
-									setError("Please enter a valid email address.");
-								} else {
-									setError("");
-								}
-
-								setValues((v) => ({ ...v, email: value }));
-							}}
-							isInvalid={!!error}
-							errorMessage={error}
-							onKeyUp={(e) => {
-								if (e.key === "Enter") {
-									handleSubmit();
-								}
-							}}
-						/>,
-
-						<>
-							<InputOtp
-								length={6}
-								size="lg"
-								value={values.otp}
-								name="otp"
-								isInvalid={!!error}
-								allowedKeys="^[0-9a-zA-Z]+$"
-								onValueChange={(val) => {
-									if (val.trim().length === 4) {
-										setError("");
-									}
-									setValues((v) => ({ ...v, otp: val }));
-								}}
-								className="mx-auto text-[30px] font-bold px-1 py-1.5"
-								classNames={{
-									segmentWrapper:
-										"flex flex-wrap justify-center gap-1 sm:gap-3 md:gap-5",
-								}}
-								isReadOnly={loading}
-								errorMessage={error}
-							/>
-						</>,
-						<>
+				<div className="space-y-5 md:mt-5 ">
+					{
+						[
 							<Input
-								className=""
-								label="Password"
-								type={showPassword ? "text" : "password"}
-								name="password"
-								isReadOnly={loading}
+								key="email"
+								name="email"
+								label="Email"
 								fullWidth
-								isInvalid={!!error}
-								value={values.password}
-								onValueChange={(value) => {
-									if (value.trim() !== values.password.trim()) {
-										setError("Password doesn't match!");
-									} else if (value.trim().length < 8) {
-										setError("Password must be at least 8 characters.");
-									} else {
-										setError("");
-									}
-									setValues((v) => ({ ...v, password: value }));
-								}}
-								radius="sm"
-								errorMessage={error}
-								endContent={
-									<Icon
-										icon={
-											showPassword ? "solar:eye-bold" : "solar:eye-closed-bold"
-										}
-										onClick={handleShowPassword}
-										className="cursor-pointer"
-									/>
-								}
-							/>
-							<Input
-								className=""
-								label="Confirm Password"
-								type={showPassword ? "text" : "password"}
-								name="confirmPassword"
+								value={values.email}
 								isReadOnly={loading}
-								isInvalid={!!error}
-								value={values.confirmPassword}
 								onValueChange={(value) => {
-									if (value.trim() !== values.password.trim()) {
-										setError("Password doesn't match!");
+									const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+									if (!emailRegex.test(value)) {
+										setError("Please enter a valid email address.");
 									} else {
 										setError("");
 									}
-									setValues((v) => ({ ...v, confirmPassword: value }));
+
+									setValues((v) => ({ ...v, email: value }));
 								}}
-								radius="sm"
+								isInvalid={!!error}
 								errorMessage={error}
-								endContent={
-									<Icon
-										icon={
-											showPassword ? "solar:eye-bold" : "solar:eye-closed-bold"
+								onKeyUp={(e) => {
+									if (e.key === "Enter") {
+										handleSubmit();
+									}
+								}}
+							/>,
+
+							<>
+								<InputOtp
+									length={6}
+									size="lg"
+									value={values.otp}
+									name="otp"
+									isInvalid={!!error}
+									allowedKeys="^[0-9a-zA-Z]+$"
+									onValueChange={(val) => {
+										if (val.trim().length === 4) {
+											setError("");
 										}
-										onClick={handleShowPassword}
-										className="cursor-pointer"
-									/>
-								}
-							/>
-						</>,
-					][step - 1]
-				}
-				{step === 2 && (
-					<div className="flex items-center flex-wrap justify-center gap-0.5">
-						<p className="text-gray-500">Don&apos; receive the email?</p>
-						<button
-							onClick={handleSubmit}
-							className="bg-none border-none outline-none p-0 text-blue-400 underline cursor-pointer">
-							Click to resend
-						</button>
-					</div>
-				)}
-				{step < 4 && (
+										setValues((v) => ({ ...v, otp: val }));
+									}}
+									className="mx-auto text-[30px] font-bold px-1 py-1.5"
+									classNames={{
+										segmentWrapper:
+											"flex flex-wrap justify-center gap-1 sm:gap-3 md:gap-5",
+									}}
+									isReadOnly={loading}
+									errorMessage={error}
+								/>
+							</>,
+							<>
+								<Input
+									className=""
+									label="Password"
+									type={showPassword ? "text" : "password"}
+									name="password"
+									isReadOnly={loading}
+									fullWidth
+									isInvalid={!!error}
+									value={values.password}
+									onValueChange={(value) => {
+										if (value.trim() !== values.password.trim()) {
+											setError("Password doesn't match!");
+										} else if (value.trim().length < 8) {
+											setError("Password must be at least 8 characters.");
+										} else {
+											setError("");
+										}
+										setValues((v) => ({ ...v, password: value }));
+									}}
+									radius="sm"
+									errorMessage={error}
+									endContent={
+										<Icon
+											icon={
+												showPassword
+													? "solar:eye-bold"
+													: "solar:eye-closed-bold"
+											}
+											onClick={handleShowPassword}
+											className="cursor-pointer"
+										/>
+									}
+								/>
+								<Input
+									className=""
+									label="Confirm Password"
+									type={showPassword ? "text" : "password"}
+									name="confirmPassword"
+									isReadOnly={loading}
+									isInvalid={!!error}
+									value={values.confirmPassword}
+									onValueChange={(value) => {
+										if (value.trim() !== values.password.trim()) {
+											setError("Password doesn't match!");
+										} else {
+											setError("");
+										}
+										setValues((v) => ({ ...v, confirmPassword: value }));
+									}}
+									radius="sm"
+									errorMessage={error}
+									endContent={
+										<Icon
+											icon={
+												showPassword
+													? "solar:eye-bold"
+													: "solar:eye-closed-bold"
+											}
+											onClick={handleShowPassword}
+											className="cursor-pointer"
+										/>
+									}
+								/>
+							</>,
+						][step - 1]
+					}
+					{step === 2 && (
+						<div className="flex items-center flex-wrap justify-center gap-0.5">
+							<p className="text-gray-500">Don&apos; receive the email?</p>
+							<button
+								onClick={handleSubmit}
+								className="bg-none border-none outline-none p-0 text-blue-400 underline cursor-pointer">
+								Click to resend
+							</button>
+						</div>
+					)}
+					{step < 4 && (
+						<Button
+							disabled={loading}
+							onPress={
+								[handleSubmit, handleVerifyOTP, handleResetPassword][step - 1]
+							}
+							isDisabled={loading}
+							isLoading={loading}
+							className="block"
+							fullWidth
+							style={{ textTransform: "capitalize" }}>
+							{["Continue", "Continue", "Reset Password"][step - 1]}
+						</Button>
+					)}
 					<Button
 						disabled={loading}
-						onPress={
-							[handleSubmit, handleVerifyOTP, handleResetPassword][step - 1]
-						}
 						isDisabled={loading}
-						isLoading={loading}
-						className="block"
+						variant={step === 4 ? "solid" : "light"}
+						startContent={<Icon icon="solar:square-arrow-left-bold" />}
+						onPress={() => {
+							navigate("/login", {
+								replace: true,
+							});
+						}}
 						fullWidth
-						style={{ textTransform: "capitalize" }}>
-						{["Continue", "Continue", "Reset Password"][step - 1]}
+						className={`capitalize  ${step === 4 ? "white" : "text-gray-500"} mt-3`}
+						type="submit">
+						{step < 4 ? "Back to login" : "Continue to Login"}
 					</Button>
-				)}
-				<Button
-					disabled={loading}
-					isDisabled={loading}
-					variant={step === 4 ? "solid" : "light"}
-					startContent={<Icon icon="solar:square-arrow-left-bold" />}
-					onPress={() => {
-						navigate("/login", {
-							replace: true,
-						});
-					}}
-					fullWidth
-					className={`capitalize  ${step === 4 ? "white" : "text-gray-500"} mt-3`}
-					type="submit">
-					{step < 4 ? "Back to login" : "Continue to Login"}
-				</Button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- client/src/pages/auth/student/login.tsx
+++ client/src/pages/auth/student/login.tsx
@@ -10,6 +10,7 @@ import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { toast } from "sonner";
 import { useSetAtom } from "jotai";
+import { Helmet } from "react-helmet";
 
 import { useAuth } from "@/contexts";
 import { verifyTOTPMutation } from "@/queries";
@@ -74,196 +75,208 @@ export default function StudentLogin() {
 	// }, []);
 
 	return (
-		<div className=" rounded-none shadow-md min-w-[90%] w-[90%] md:max-w-[680px] md:min-w-[380px] bg-[#A6F3B228] ">
-			<Formik
-				initialValues={{ email: "", password: "", otp: "" }}
-				onSubmit={async (values) => {
-					if (mfaEnabled) {
-						verifyTOTP({
-							variables: {
-								secret: studentUser?.mfaSecret,
-								token: values.otp,
-							},
-						});
-					} else {
-						try {
-							const response = await fetch(
-								`${import.meta.env.VITE_API_URL}/api/auth/login`,
-								{
-									method: "POST",
-									body: JSON.stringify(values),
-									headers: {
-										"Content-type": "application/json",
-									},
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Log in | SEDP</title>
+				<meta
+					name="description"
+					content="Log in to the SEDP student portal to manage your system."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Helmet>
+			<div className=" rounded-none shadow-md min-w-[90%] w-[90%] md:max-w-[680px] md:min-w-[380px] bg-[#A6F3B228] ">
+				<Formik
+					initialValues={{ email: "", password: "", otp: "" }}
+					onSubmit={async (values) => {
+						if (mfaEnabled) {
+							verifyTOTP({
+								variables: {
+									secret: studentUser?.mfaSecret,
+									token: values.otp,
+								},
+							});
+						} else {
+							try {
+								const response = await fetch(
+									`${import.meta.env.VITE_API_URL}/api/auth/login`,
+									{
+										method: "POST",
+										body: JSON.stringify(values),
+										headers: {
+											"Content-type": "application/json",
+										},
+									}
+								);
+								const data = await response.json();
+
+								if (response.status !== 200) {
+									throw new Error(data.error.message);
 								}
-							);
-							const data = await response.json();
 
-							if (response.status !== 200) {
-								throw new Error(data.error.message);
-							}
+								if (data.data.user.status === "DISQUALIFIED") {
+									return navigate("/disqualify", {
+										replace: true,
+									});
+								}
 
-							if (data.data.user.status === "DISQUALIFIED") {
-								return navigate("/disqualify", {
-									replace: true,
-								});
-							}
+								if (data.data.user.mfaEnabled) {
+									setMfaEnabled(true);
+									localStorage.setItem("isLoggedIn", "false");
 
-							if (data.data.user.mfaEnabled) {
-								setMfaEnabled(true);
-								localStorage.setItem("isLoggedIn", "false");
+									login(data.data.accessToken, false);
+								} else {
+									localStorage.removeItem("isLoggedIn");
+									toast.success("Successfully logged in", {
+										richColors: true,
+										position: "top-center",
+										dismissible: true,
+										duration: 5000,
+										icon: <Icon icon="clarity:error-solid" />,
+									});
+									navigate("/", {
+										replace: true,
+									});
 
-								login(data.data.accessToken, false);
-							} else {
-								localStorage.removeItem("isLoggedIn");
-								toast.success("Successfully logged in", {
+									login(data.data.accessToken);
+								}
+								const { notifications, ...userData } = data.data.user;
+
+								setStudentUser(userData);
+								setNotifications(notifications || []);
+							} catch (err) {
+								toast.error((err as Error).message, {
 									richColors: true,
 									position: "top-center",
 									dismissible: true,
 									duration: 5000,
 									icon: <Icon icon="clarity:error-solid" />,
 								});
-								navigate("/", {
-									replace: true,
-								});
-
-								login(data.data.accessToken);
 							}
-							const { notifications, ...userData } = data.data.user;
-
-							setStudentUser(userData);
-							setNotifications(notifications || []);
-						} catch (err) {
-							toast.error((err as Error).message, {
-								richColors: true,
-								position: "top-center",
-								dismissible: true,
-								duration: 5000,
-								icon: <Icon icon="clarity:error-solid" />,
-							});
 						}
-					}
-				}}
-				validateOnBlur
-				validateOnChange
-				validationSchema={validationSchema}>
-				{({
-					errors,
-					handleChange,
-					values,
-					handleBlur,
-					handleSubmit,
-					touched,
-					isSubmitting,
-					resetForm,
-				}) => {
-					return (
-						<Form onSubmit={handleSubmit}>
-							<div className="mb-3 h-[86px] bg-[#A6F3B2] flex items-center justify-center">
-								<h1 className="text-xl">LOGIN TO YOUR ACCOUNT</h1>
-							</div>
-							<div className="space-y-4 px-5 md:px-14 py-10">
-								{!mfaEnabled ? (
-									<>
-										<Input
-											label="Email"
-											type="email"
-											name="email"
-											isReadOnly={isSubmitting}
-											value={values.email}
-											radius="sm"
-											isInvalid={!!errors.email}
-											onChange={handleChange}
-											onBlur={handleBlur}
-											errorMessage={touched.email && errors.email}
-										/>
-										<div className="space-y-2">
+					}}
+					validateOnBlur
+					validateOnChange
+					validationSchema={validationSchema}>
+					{({
+						errors,
+						handleChange,
+						values,
+						handleBlur,
+						handleSubmit,
+						touched,
+						isSubmitting,
+						resetForm,
+					}) => {
+						return (
+							<Form onSubmit={handleSubmit}>
+								<div className="mb-3 h-[86px] bg-[#A6F3B2] flex items-center justify-center">
+									<h1 className="text-xl">LOGIN TO YOUR ACCOUNT</h1>
+								</div>
+								<div className="space-y-4 px-5 md:px-14 py-10">
+									{!mfaEnabled ? (
+										<>
 											<Input
-												className=""
-												label="Password"
-												type={showPassword ? "text" : "password"}
-												name="password"
+												label="Email"
+												type="email"
+												name="email"
 												isReadOnly={isSubmitting}
-												value={values.password}
-												isInvalid={!!errors.password}
+												value={values.email}
+												radius="sm"
+												isInvalid={!!errors.email}
 												onChange={handleChange}
 												onBlur={handleBlur}
-												radius="sm"
-												errorMessage={touched.password && errors.password}
-												endContent={
-													<Icon
-														icon={
-															showPassword
-																? "solar:eye-bold"
-																: "solar:eye-closed-bold"
-														}
-														onClick={handleShowPassword}
-														className="cursor-pointer"
-													/>
-												}
+												errorMessage={touched.email && errors.email}
 											/>
-											<Link
-												className="text-right hover:underline  block"
-												to={
-													mutating || isSubmitting ? "#" : "/forgot-password"
-												}>
-												Forgot Password?
-											</Link>
-										</div>
-									</>
-								) : (
-									<>
-										<div>
-											<h1 className="text-md font-medium">
-												Enter Your Two-Factor Authentication Code
-											</h1>
-											<p className="text-sm text-gray-500">
-												Open your authenticator app (Google Authenticator or
-												similar), and enter the code generated for your account.
-												This code changes every 30 seconds for added security
-											</p>
-										</div>
-										<InputOtp
-											length={6}
-											size="lg"
-											value={values.otp}
-											name="otp"
-											onChange={handleChange}
-											className="mx-auto "
-											readOnly={isSubmitting || mutating}
-										/>
-									</>
-								)}
-								<div className="space-y-1">
-									<Button
-										isLoading={isSubmitting || mutating}
-										type="submit"
-										className="bg-[#A6F3B2]"
-										fullWidth>
-										{mfaEnabled ? "Verify OTP" : "Login"}
-									</Button>
-
-									{mfaEnabled && (
-										<Button
-											disabled={isSubmitting || mutating}
-											onPress={() => {
-												setMfaEnabled(false);
-												resetForm();
-												localStorage.clear();
-												setStudentUser(null);
-											}}
-											fullWidth
-											type="submit"
-											variant="light">
-											Back to Log in
-										</Button>
+											<div className="space-y-2">
+												<Input
+													className=""
+													label="Password"
+													type={showPassword ? "text" : "password"}
+													name="password"
+													isReadOnly={isSubmitting}
+													value={values.password}
+													isInvalid={!!errors.password}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													radius="sm"
+													errorMessage={touched.password && errors.password}
+													endContent={
+														<Icon
+															icon={
+																showPassword
+																	? "solar:eye-bold"
+																	: "solar:eye-closed-bold"
+															}
+															onClick={handleShowPassword}
+															className="cursor-pointer"
+														/>
+													}
+												/>
+												<Link
+													className="text-right hover:underline  block"
+													to={
+														mutating || isSubmitting ? "#" : "/forgot-password"
+													}>
+													Forgot Password?
+												</Link>
+											</div>
+										</>
+									) : (
+										<>
+											<div>
+												<h1 className="text-md font-medium">
+													Enter Your Two-Factor Authentication Code
+												</h1>
+												<p className="text-sm text-gray-500">
+													Open your authenticator app (Google Authenticator or
+													similar), and enter the code generated for your
+													account. This code changes every 30 seconds for added
+													security
+												</p>
+											</div>
+											<InputOtp
+												length={6}
+												size="lg"
+												value={values.otp}
+												name="otp"
+												onChange={handleChange}
+												className="mx-auto "
+												readOnly={isSubmitting || mutating}
+											/>
+										</>
 									)}
+									<div className="space-y-1">
+										<Button
+											isLoading={isSubmitting || mutating}
+											type="submit"
+											className="bg-[#A6F3B2]"
+											fullWidth>
+											{mfaEnabled ? "Verify OTP" : "Login"}
+										</Button>
+
+										{mfaEnabled && (
+											<Button
+												disabled={isSubmitting || mutating}
+												onPress={() => {
+													setMfaEnabled(false);
+													resetForm();
+													localStorage.clear();
+													setStudentUser(null);
+												}}
+												fullWidth
+												type="submit"
+												variant="light">
+												Back to Log in
+											</Button>
+										)}
+									</div>
 								</div>
-							</div>
-						</Form>
-					);
-				}}
-			</Formik>
-		</div>
+							</Form>
+						);
+					}}
+				</Formik>
+			</div>
+		</>
 	);
 }
