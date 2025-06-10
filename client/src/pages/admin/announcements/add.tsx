25f0145284051cf1dc76ee28798feb1166d586e7
awynx <alwin.puche16@gmail.com>
2025-06-10 21:58:10 +0800

    feat: add Helmet for SEO in AddAnnouncement, EditAnnouncement, AnnouncementInfo, and Announcements components

--- client/src/pages/admin/announcements/add.tsx
+++ client/src/pages/admin/announcements/add.tsx
@@ -1,22 +1,35 @@
 import { Card, CardBody, CardHeader } from "@heroui/card";
+import { Helmet } from "react-helmet";
 
 import AnnouncementForm from "./__components/form";
 
 export default function AddAnnounceMent() {
 	return (
-		<Card className="rounded-md shadow-md mb-10 ">
-			<CardHeader className="flex rounded-none bg-[#A6F3B2] flex-col items-start">
-				<h1 className="text-2xl">Create Announcement</h1>
-				<p>
-					Create a new announcement to inform scholars about important updates.
-				</p>
-			</CardHeader>
-			<CardBody className="bg-[#A6F3B235]">
-				<div className="lg:max-w-[80%] w-full mx-auto my-5">
-					<AnnouncementForm />
-				</div>
-			</CardBody>
-		</Card>
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Create Announcement | Admin</title>
+				<meta
+					name="description"
+					content="Create a new announcement to inform scholars about important updates."
+				/>
+			</Helmet>
+			<Card className="rounded-md shadow-md mb-10 ">
+				<CardHeader className="flex rounded-none bg-[#A6F3B2] flex-col items-start">
+					<h1 className="text-2xl">Create Announcement</h1>
+					<p>
+						Create a new announcement to inform scholars about important
+						updates.
+					</p>
+				</CardHeader>
+				<CardBody className="bg-[#A6F3B235]">
+					<div className="lg:max-w-[80%] w-full mx-auto my-5">
+						<AnnouncementForm />
+					</div>
+				</CardBody>
+			</Card>
+		</>
 	);
 }
 
--- client/src/pages/admin/announcements/info.tsx
+++ client/src/pages/admin/announcements/info.tsx
@@ -5,6 +5,7 @@ import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
 import { Divider } from "@heroui/divider";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useNavigate, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import { READ_ANNOUNCEMENT_QUERY } from "@/queries";
 import { Announcement } from "@/types";
@@ -30,53 +31,63 @@ export default function AnnouncementInfo() {
 	const announcement = data.announcement;
 
 	return (
-		<div className="max-w-screen-lg mx-auto p-6">
-			<div className="flex flex-col gap-2 mb-6">
-				<Button
-					variant="flat"
-					size="sm"
-					color="success"
-					className="max-w-fit"
-					startContent={<Icon icon="ep:back" />}
-					onPress={() => navigate("/admin/announcements")}>
-					Back to List
-				</Button>
-				<h1 className="text-2xl font-bold">Announcement Details</h1>
-			</div>
-
-			<Card className="w-full" shadow="sm">
-				<CardHeader className="flex justify-between">
-					<div className="flex items-center gap-3">
-						<Avatar
-							src={`https://i.pravatar.cc/150`}
-							name={`${announcement.createdBy.firstName} ${announcement.createdBy.lastName}`}
-							size="sm"
-						/>
-						<div className="flex flex-col">
-							<p className="text-small font-semibold">
-								{`${announcement.createdBy.firstName} ${announcement.createdBy.lastName}`}
-							</p>
-							<p className="text-tiny text-default-500">
-								{announcement.createdBy.email}
-							</p>
-						</div>
-					</div>
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Announcement Info | Admin</title>
+				<meta
+					name="description"
+					content="View the details of an announcement, including its content and the author."
+				/>
+			</Helmet>
+			<div className="max-w-screen-lg mx-auto p-6">
+				<div className="flex flex-col gap-2 mb-6">
 					<Button
 						variant="flat"
+						size="sm"
 						color="success"
-						isDisabled={
-							!["SUPER_ADMIN", "ADMIN_MANAGE_ANNOUNCEMENTS"].includes(role!)
-						}
-						className="disabled:cursor-not-allowed"
-						startContent={<Icon icon="lucide:edit" />}
-						onPress={() =>
-							navigate(`/admin/announcements/${announcement.id}/edit`)
-						}>
-						Edit
+						className="max-w-fit"
+						startContent={<Icon icon="ep:back" />}
+						onPress={() => navigate("/admin/announcements")}>
+						Back to List
 					</Button>
-				</CardHeader>
-				<CardBody className="gap-6">
-					{/* 	<Button
+					<h1 className="text-2xl font-bold">Announcement Details</h1>
+				</div>
+
+				<Card className="w-full" shadow="sm">
+					<CardHeader className="flex justify-between">
+						<div className="flex items-center gap-3">
+							<Avatar
+								src={`https://i.pravatar.cc/150`}
+								name={`${announcement.createdBy.firstName} ${announcement.createdBy.lastName}`}
+								size="sm"
+							/>
+							<div className="flex flex-col">
+								<p className="text-small font-semibold">
+									{`${announcement.createdBy.firstName} ${announcement.createdBy.lastName}`}
+								</p>
+								<p className="text-tiny text-default-500">
+									{announcement.createdBy.email}
+								</p>
+							</div>
+						</div>
+						<Button
+							variant="flat"
+							color="success"
+							isDisabled={
+								!["SUPER_ADMIN", "ADMIN_MANAGE_ANNOUNCEMENTS"].includes(role!)
+							}
+							className="disabled:cursor-not-allowed"
+							startContent={<Icon icon="lucide:edit" />}
+							onPress={() =>
+								navigate(`/admin/announcements/${announcement.id}/edit`)
+							}>
+							Edit
+						</Button>
+					</CardHeader>
+					<CardBody className="gap-6">
+						{/* 	<Button
 								variant="flat"
 								color="danger"
 								startContent={<Icon icon="lucide:trash" />}
@@ -84,22 +95,23 @@ export default function AnnouncementInfo() {
 								Delete
 							</Button> */}
 
-					<Divider />
+						<Divider />
 
-					<div className="space-y-4">
-						<p className="text-gray-500 text-xs">Content</p>
-						<p className="px-5 text-default-500 whitespace-pre-wrap">
-							{announcement.content}
+						<div className="space-y-4">
+							<p className="text-gray-500 text-xs">Content</p>
+							<p className="px-5 text-default-500 whitespace-pre-wrap">
+								{announcement.content}
+							</p>
+						</div>
+					</CardBody>
+					<CardFooter>
+						<p className="text-small text-default-400">
+							<Icon icon="lucide:clock" className="inline-block mr-1" />
+							Posted on {formatDate(announcement.createdAt)}
 						</p>
-					</div>
-				</CardBody>
-				<CardFooter>
-					<p className="text-small text-default-400">
-						<Icon icon="lucide:clock" className="inline-block mr-1" />
-						Posted on {formatDate(announcement.createdAt)}
-					</p>
-				</CardFooter>
-			</Card>
-		</div>
+					</CardFooter>
+				</Card>
+			</div>
+		</>
 	);
 }
