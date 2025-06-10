6d06de2b812facb0227fff07be28177b7cb193d9
awynx <alwin.puche16@gmail.com>
2025-06-11 01:53:02 +0800

    feat: add Helmet for SEO in SemesterSubmissions and StudentSemesterFiles components

--- client/src/pages/admin/semester-docs/scholar/info.tsx
+++ client/src/pages/admin/semester-docs/scholar/info.tsx
@@ -10,6 +10,7 @@ import { Button } from "@heroui/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
+import { Helmet } from "react-helmet";
 
 import EditModal from "./_components/edit-modal";
 
@@ -121,149 +122,160 @@ export default function StudentSemesterFiles() {
 	// const selectedYear = Number(Array.from(yearFilter)[0]);
 
 	return (
-		<div className="container mx-auto  py-5">
-			<div className="flex-col md:flex-row flex  justify-between items-center ">
-				<div>
-					<h1 className="text-2xl">
-						{scholar.firstName} {scholar.lastName}&apos;s <span>files</span>
-					</h1>
-					<p className="md:max-w-2xl text-sm text-default-400">
-						The list below displays the files submitted by the student for the
-						semester submission process. Please click on a file to view more
-						details.
-					</p>
-				</div>
-				<div className=" w-full md:w-auto py-4 md:py-0 flex gap-2 items-center">
-					<Dropdown
-						classNames={{
-							content: "bg-[#A6F3B2]",
-						}}>
-						<DropdownTrigger className=" flex bg-[#A6F3B2]">
-							<Button
-								endContent={
-									<Icon
-										icon="mynaui:chevron-down-solid"
-										width="24"
-										height="24"
-									/>
-								}
-								size="md"
-								variant="flat">
-								{isNaN(Number(Array.from(yearFilter)[0]))
-									? "Select School Year"
-									: `S.Y. ${Number(Array.from(yearFilter)[0])} - ${Number(Array.from(yearFilter)[0]) + 1}`}
-							</Button>
-						</DropdownTrigger>
-						<DropdownMenu
-							// onSelectionChange={(year) => {
-							// 	if (!year) return;
-							// 	setYearFilter(Number(year.currentKey));
-							// }}
-							onSelectionChange={(key) => {
-								if (!key) return;
-
-								const keyString = Array.from(key)[0].toString().split("-")[0];
-								const keySet = new Set([keyString]);
-
-								setSelectedYear(Array.from(key)[0].toString().split("-")[1]);
-								setYearFilter(keySet);
-							}}
-							selectedKeys={yearFilter}
-							aria-label="Year Filter"
-							selectionMode="single">
-							{years.map((year) => (
-								<DropdownItem
-									key={year}
-									className="data-[focus=true]:!bg-[#1f4e26] data-[focus=true]:!text-white capitalize">
-									{Number(year.split("-")[0])} -{" "}
-									{Number(year.split("-")[0]) + 1}
-								</DropdownItem>
-							))}
-						</DropdownMenu>
-					</Dropdown>
-					<Dropdown
-						classNames={{
-							content: "bg-[#A6F3B2]",
-						}}>
-						<DropdownTrigger className="  bg-[#A6F3B2]">
-							<Button
-								endContent={
-									<Icon
-										icon="mynaui:chevron-down-solid"
-										width="24"
-										height="24"
-									/>
-								}
-								size="md"
-								variant="flat">
-								{semester[selectedSemester - 1]}
-							</Button>
-						</DropdownTrigger>
-						<DropdownMenu
-							// onSelectionChange={(year) => {
-							// 	if (!year) return;
-							// 	setYearFilter(Number(year.currentKey));
-							// }}
-							disallowEmptySelection
-							onSelectionChange={setSemesterFilter}
-							selectedKeys={semesterFilter}
-							aria-label="Year Filter"
-							selectionMode="single">
-							{[1, 2, 3].map((year) => (
-								<DropdownItem
-									key={year}
-									className="data-[focus=true]:!bg-[#1f4e26] data-[focus=true]:!text-white capitalize">
-									{semester[year - 1]}
-								</DropdownItem>
-							))}
-						</DropdownMenu>
-					</Dropdown>
-				</div>
-			</div>
-			<div className="relative px-0.5">
-				{isNaN(Number(Array.from(yearFilter)[0])) && (
-					<div className="absolute min-h-[300px] top-0 left-0 backdrop-blur-sm flex justify-center items-center  z-10 h-full w-full">
-						<h1 className="text-2xl font-semibold text-gray-800">
-							Select a School Year to Fetch Documents
-						</h1>
-					</div>
-				)}
-				<DocumentTable
-					data={data?.documents || []}
-					isLoading={loading}
-					handleRowClick={(url) => {
-						onPreviewModalChange(true);
-						setToPreview(url);
-					}}
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Scholar Semester Document | Admin</title>
+				<meta
+					name="description"
+					content="View the semester documents submitted by a scholar in the admin panel."
 				/>
-				{toPreview && (
-					<PreviewModal
-						src={toPreview}
-						type={
-							imagesExtensions.includes(getFileExtension(toPreview) || "png")
-								? `image`
-								: `document`
-						}
-						isOpen={previewModal}
-						onOpenChange={onPreviewModalChange}
-					/>
-				)}
-			</div>
-
-			{!isNaN(Number(Array.from(yearFilter)[0])) &&
-				(Number(selectedYear) > scholar.yearLevel ||
-					(Number(selectedYear) === scholar.yearLevel &&
-						selectedSemester > scholar.semester)) &&
-				(data?.documents.length || 0) > 0 &&
-				scholar.status === "SCHOLAR" && (
-					<div className="absolute bottom-0 left-0 right-0 bg-white p-5 md:px-10 flex justify-between items-center">
-						<EditModal
-							scholar={scholar}
-							semester={selectedSemester}
-							yearLevel={Number(selectedYear)}
-						/>
+			</Helmet>
+			<div className="container mx-auto  py-5">
+				<div className="flex-col md:flex-row flex  justify-between items-center ">
+					<div>
+						<h1 className="text-2xl">
+							{scholar.firstName} {scholar.lastName}&apos;s <span>files</span>
+						</h1>
+						<p className="md:max-w-2xl text-sm text-default-400">
+							The list below displays the files submitted by the student for the
+							semester submission process. Please click on a file to view more
+							details.
+						</p>
 					</div>
-				)}
-		</div>
+					<div className=" w-full md:w-auto py-4 md:py-0 flex gap-2 items-center">
+						<Dropdown
+							classNames={{
+								content: "bg-[#A6F3B2]",
+							}}>
+							<DropdownTrigger className=" flex bg-[#A6F3B2]">
+								<Button
+									endContent={
+										<Icon
+											icon="mynaui:chevron-down-solid"
+											width="24"
+											height="24"
+										/>
+									}
+									size="md"
+									variant="flat">
+									{isNaN(Number(Array.from(yearFilter)[0]))
+										? "Select School Year"
+										: `S.Y. ${Number(Array.from(yearFilter)[0])} - ${Number(Array.from(yearFilter)[0]) + 1}`}
+								</Button>
+							</DropdownTrigger>
+							<DropdownMenu
+								// onSelectionChange={(year) => {
+								// 	if (!year) return;
+								// 	setYearFilter(Number(year.currentKey));
+								// }}
+								onSelectionChange={(key) => {
+									if (!key) return;
+
+									const keyString = Array.from(key)[0].toString().split("-")[0];
+									const keySet = new Set([keyString]);
+
+									setSelectedYear(Array.from(key)[0].toString().split("-")[1]);
+									setYearFilter(keySet);
+								}}
+								selectedKeys={yearFilter}
+								aria-label="Year Filter"
+								selectionMode="single">
+								{years.map((year) => (
+									<DropdownItem
+										key={year}
+										className="data-[focus=true]:!bg-[#1f4e26] data-[focus=true]:!text-white capitalize">
+										{Number(year.split("-")[0])} -{" "}
+										{Number(year.split("-")[0]) + 1}
+									</DropdownItem>
+								))}
+							</DropdownMenu>
+						</Dropdown>
+						<Dropdown
+							classNames={{
+								content: "bg-[#A6F3B2]",
+							}}>
+							<DropdownTrigger className="  bg-[#A6F3B2]">
+								<Button
+									endContent={
+										<Icon
+											icon="mynaui:chevron-down-solid"
+											width="24"
+											height="24"
+										/>
+									}
+									size="md"
+									variant="flat">
+									{semester[selectedSemester - 1]}
+								</Button>
+							</DropdownTrigger>
+							<DropdownMenu
+								// onSelectionChange={(year) => {
+								// 	if (!year) return;
+								// 	setYearFilter(Number(year.currentKey));
+								// }}
+								disallowEmptySelection
+								onSelectionChange={setSemesterFilter}
+								selectedKeys={semesterFilter}
+								aria-label="Year Filter"
+								selectionMode="single">
+								{[1, 2, 3].map((year) => (
+									<DropdownItem
+										key={year}
+										className="data-[focus=true]:!bg-[#1f4e26] data-[focus=true]:!text-white capitalize">
+										{semester[year - 1]}
+									</DropdownItem>
+								))}
+							</DropdownMenu>
+						</Dropdown>
+					</div>
+				</div>
+				<div className="relative px-0.5">
+					{isNaN(Number(Array.from(yearFilter)[0])) && (
+						<div className="absolute min-h-[300px] top-0 left-0 backdrop-blur-sm flex justify-center items-center  z-10 h-full w-full">
+							<h1 className="text-2xl font-semibold text-gray-800">
+								Select a School Year to Fetch Documents
+							</h1>
+						</div>
+					)}
+					<DocumentTable
+						data={data?.documents || []}
+						isLoading={loading}
+						handleRowClick={(url) => {
+							onPreviewModalChange(true);
+							setToPreview(url);
+						}}
+					/>
+					{toPreview && (
+						<PreviewModal
+							src={toPreview}
+							type={
+								imagesExtensions.includes(getFileExtension(toPreview) || "png")
+									? `image`
+									: `document`
+							}
+							isOpen={previewModal}
+							onOpenChange={onPreviewModalChange}
+						/>
+					)}
+				</div>
+
+				{!isNaN(Number(Array.from(yearFilter)[0])) &&
+					(Number(selectedYear) > scholar.yearLevel ||
+						(Number(selectedYear) === scholar.yearLevel &&
+							selectedSemester > scholar.semester)) &&
+					(data?.documents.length || 0) > 0 &&
+					scholar.status === "SCHOLAR" && (
+						<div className="absolute bottom-0 left-0 right-0 bg-white p-5 md:px-10 flex justify-between items-center">
+							<EditModal
+								scholar={scholar}
+								semester={selectedSemester}
+								yearLevel={Number(selectedYear)}
+							/>
+						</div>
+					)}
+			</div>
+		</>
 	);
 }
