3e1998b959b5b2f6672a052ecf951ae565e37d84
awynx <alwin.puche16@gmail.com>
2025-09-18 21:55:45 +0800

    Fix error handling in eventsResolver to show error message

--- server/src/graphql/resolvers/event.ts
+++ server/src/graphql/resolvers/event.ts
@@ -1,162 +1,164 @@
 import { Events } from "@prisma/client";
 import {
-	deleteEvent,
-	readAllEvents,
-	readEvent,
-	readEventAsCalendar,
-	readMonthlyEvents,
-	upsertEvent,
+  deleteEvent,
+  readAllEvents,
+  readEvent,
+  readEventAsCalendar,
+  readMonthlyEvents,
+  upsertEvent,
 } from "../../models/event.js";
 import { AppContext, PaginationArgs } from "../../types/index.js";
 import { GraphQLError } from "graphql";
 
 export const createEventResolver = async (
-	_: never,
-	{
-		description,
-		endDate,
-		endTime,
-		location,
-		startDate,
-		startTime,
-		title,
-	}: Omit<Events, "createdAt" | "id">,
-	app: AppContext
+  _: never,
+  {
+    description,
+    endDate,
+    endTime,
+    location,
+    startDate,
+    startTime,
+    title,
+  }: Omit<Events, "createdAt" | "id">,
+  app: AppContext,
 ) => {
-	try {
-		if (
-			!(app.role === "SUPER_ADMIN" || app.role === "ADMIN_MANAGE_GATHERINGS")
-		) {
-			throw new GraphQLError("Unauthorized!");
-		}
+  try {
+    if (
+      !(app.role === "SUPER_ADMIN" || app.role === "ADMIN_MANAGE_GATHERINGS")
+    ) {
+      throw new GraphQLError("Unauthorized!");
+    }
 
-		const newEvent = await upsertEvent(
-			{
-				description,
-				endDate,
-				endTime,
-				location,
-				startDate,
-				startTime,
-				title,
-			},
-			"67dfd118d8898db58de87455"
-		);
+    const newEvent = await upsertEvent(
+      {
+        description,
+        endDate,
+        endTime,
+        location,
+        startDate,
+        startTime,
+        title,
+      },
+      "67dfd118d8898db58de87455",
+    );
 
-		if (!newEvent) throw new GraphQLError("Failed to create event!");
+    if (!newEvent) throw new GraphQLError("Failed to create event!");
 
-		return newEvent;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return newEvent;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
 
 export const updateEventResolver = async (
-	_: never,
-	{
-		description,
-		endDate,
-		endTime,
-		location,
-		startDate,
-		startTime,
-		title,
-		id,
-	}: Omit<Events, "createdAt">,
-	app: AppContext
+  _: never,
+  {
+    description,
+    endDate,
+    endTime,
+    location,
+    startDate,
+    startTime,
+    title,
+    id,
+  }: Omit<Events, "createdAt">,
+  app: AppContext,
 ) => {
-	try {
-		if (
-			!(app.role === "SUPER_ADMIN" || app.role === "ADMIN_MANAGE_GATHERINGS")
-		) {
-			throw new GraphQLError("Unauthorized!");
-		}
+  try {
+    if (
+      !(app.role === "SUPER_ADMIN" || app.role === "ADMIN_MANAGE_GATHERINGS")
+    ) {
+      throw new GraphQLError("Unauthorized!");
+    }
 
-		const updatedEvent = await upsertEvent(
-			{
-				description,
-				endDate,
-				endTime,
-				location,
-				startDate,
-				startTime,
-				title,
-			},
-			id
-		);
+    const updatedEvent = await upsertEvent(
+      {
+        description,
+        endDate,
+        endTime,
+        location,
+        startDate,
+        startTime,
+        title,
+      },
+      id,
+    );
 
-		if (!updatedEvent) throw new GraphQLError("Failed to create event!");
+    if (!updatedEvent) throw new GraphQLError("Failed to create event!");
 
-		return updatedEvent;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return updatedEvent;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
 
 export const eventsResolver = async (
-	_: never,
-	{ filter, pagination }: PaginationArgs<never>
+  _: never,
+  { filter, pagination }: PaginationArgs<never>,
 ) => {
-	try {
-		return await readAllEvents({
-			filter: filter ?? undefined,
-			pagination: pagination ? pagination : undefined,
-		});
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+  try {
+    return await readAllEvents({
+      filter: filter ?? undefined,
+      pagination: pagination ? pagination : undefined,
+    });
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError(
+      (err as GraphQLError).message ?? "Internal Server Error!",
+    );
+  }
 };
 
 export const eventResolver = async (_: never, { id }: { id: string }) => {
-	try {
-		const event = await readEvent(id);
+  try {
+    const event = await readEvent(id);
 
-		if (!event) throw new GraphQLError("Failed to create event!");
+    if (!event) throw new GraphQLError("Failed to create event!");
 
-		return event;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return event;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
 
 export const deleteEventResolver = async (_: never, { id }: { id: string }) => {
-	try {
-		const event = await deleteEvent(id);
+  try {
+    const event = await deleteEvent(id);
 
-		if (!event) throw new GraphQLError("Failed to create event!");
+    if (!event) throw new GraphQLError("Failed to create event!");
 
-		return event;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return event;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
 
 export const calendarEventsResolver = async () => {
-	try {
-		const events = await readEventAsCalendar();
+  try {
+    const events = await readEventAsCalendar();
 
-		return events;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return events;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
 
 export const monthlyEventsResolver = async () => {
-	try {
-		const events = await readMonthlyEvents(
-			new Date().getFullYear(),
-			new Date().getMonth()
-		);
+  try {
+    const events = await readMonthlyEvents(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+    );
 
-		return events;
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    return events;
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
