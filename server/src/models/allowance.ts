726d05604b0dd2659eb070c8c39e18bb8d3a507b
awynx <alwin.puche16@gmail.com>
2025-09-18 17:39:50 +0800

    Add debug log to readAllowances for where filter

--- server/src/models/allowance.ts
+++ server/src/models/allowance.ts
@@ -3,142 +3,144 @@ import { prisma } from "../services/prisma.js";
 import { GetAllowanceArgs } from "../types/index.js";
 
 export const createAllowance = async (
-	data: Omit<
-		Allowance,
-		"createdAt" | "updatedAt" | "id" | "claimed" | "claimedAt"
-	>
+  data: Omit<
+    Allowance,
+    "createdAt" | "updatedAt" | "id" | "claimed" | "claimedAt"
+  >,
 ) => {
-	const allowance = await prisma.allowance.create({
-		data: {
-			year: data.year,
-			month: data.month,
-			totalAmount: data.totalAmount,
-			semester: data.semester,
-			yearLevel: data.yearLevel,
-			bookAllowance: data.bookAllowance || 0,
-			miscellaneousAllowance: data.miscellaneousAllowance || 0,
-			monthlyAllowance: data.monthlyAllowance || 0,
-			thesisAllowance: data.thesisAllowance || 0,
-			student: {
-				connect: {
-					id: data.studentId,
-				},
-			},
-		},
-	});
+  const allowance = await prisma.allowance.create({
+    data: {
+      year: data.year,
+      month: data.month,
+      totalAmount: data.totalAmount,
+      semester: data.semester,
+      yearLevel: data.yearLevel,
+      bookAllowance: data.bookAllowance || 0,
+      miscellaneousAllowance: data.miscellaneousAllowance || 0,
+      monthlyAllowance: data.monthlyAllowance || 0,
+      thesisAllowance: data.thesisAllowance || 0,
+      student: {
+        connect: {
+          id: data.studentId,
+        },
+      },
+    },
+  });
 
-	return {
-		...allowance,
-		createdAt: allowance.createdAt.toISOString(),
-		updatedAt: allowance.updatedAt.toISOString(),
-	};
+  return {
+    ...allowance,
+    createdAt: allowance.createdAt.toISOString(),
+    updatedAt: allowance.updatedAt.toISOString(),
+  };
 };
 
 export const updateAllowanceStatus = async (id: string, claimed: boolean) => {
-	let claimedAt = null;
+  let claimedAt = null;
 
-	if (claimed) {
-		claimedAt = new Date();
-	}
+  if (claimed) {
+    claimedAt = new Date();
+  }
 
-	const allowance = await prisma.allowance.update({
-		where: {
-			id,
-		},
-		data: {
-			claimed,
-			claimedAt,
-		},
-	});
+  const allowance = await prisma.allowance.update({
+    where: {
+      id,
+    },
+    data: {
+      claimed,
+      claimedAt,
+    },
+  });
 
-	return {
-		...allowance,
-		createdAt: allowance.createdAt.toISOString(),
-		updatedAt: allowance.updatedAt.toISOString(),
-	};
+  return {
+    ...allowance,
+    createdAt: allowance.createdAt.toISOString(),
+    updatedAt: allowance.updatedAt.toISOString(),
+  };
 };
 
 export const readAllowance = async (
-	studentId: string,
-	year: number,
-	month: number
+  studentId: string,
+  year: number,
+  month: number,
 ) => {
-	const allowance = await prisma.allowance.findFirst({
-		where: {
-			studentId,
-			year,
-			month,
-		},
-	});
+  const allowance = await prisma.allowance.findFirst({
+    where: {
+      studentId,
+      year,
+      month,
+    },
+  });
 
-	if (!allowance) return null;
+  if (!allowance) return null;
 
-	return {
-		...allowance,
-		createdAt: allowance.createdAt.toISOString(),
-		updatedAt: allowance.updatedAt.toISOString(),
-	};
+  return {
+    ...allowance,
+    createdAt: allowance.createdAt.toISOString(),
+    updatedAt: allowance.updatedAt.toISOString(),
+  };
 };
 
 export const readAllowances = async ({
-	claimed = false,
-	month,
-	pagination,
-	semester,
-	studentId,
-	year,
-	yearLevel,
-	includeStudent,
+  claimed = false,
+  month,
+  pagination,
+  semester,
+  studentId,
+  year,
+  yearLevel,
+  includeStudent,
 }: GetAllowanceArgs) => {
-	let where: Prisma.AllowanceWhereInput = {};
+  let where: Prisma.AllowanceWhereInput = {};
 
-	if (studentId) {
-		where.studentId = studentId;
-	}
+  console.log(where, "qqq");
 
-	if (year) {
-		where.year = year;
-	}
+  if (studentId) {
+    where.studentId = studentId;
+  }
 
-	if (month) {
-		where.month = month;
-	}
-	if (semester) {
-		where.semester = semester;
-	}
-	if (yearLevel) {
-		where.yearLevel = yearLevel;
-	}
-	if (claimed) {
-		where.claimed = claimed;
-	}
+  if (year) {
+    where.year = year;
+  }
 
-	const allowances = await prisma.allowance.findMany({
-		where,
-		take: pagination ? pagination.take : undefined,
-		skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
-		include: {
-			student: includeStudent,
-		},
-	});
+  if (month) {
+    where.month = month;
+  }
+  if (semester) {
+    where.semester = semester;
+  }
+  if (yearLevel) {
+    where.yearLevel = yearLevel;
+  }
+  if (claimed) {
+    where.claimed = claimed;
+  }
 
-	const count = await prisma.allowance.count({
-		where,
-		take: pagination ? pagination.take : undefined,
-		skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
-	});
+  const allowances = await prisma.allowance.findMany({
+    where,
+    take: pagination ? pagination.take : undefined,
+    skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
+    include: {
+      student: includeStudent,
+    },
+  });
 
-	const hasMore = pagination
-		? pagination.page * pagination.take < count
-		: false;
+  const count = await prisma.allowance.count({
+    where,
+    take: pagination ? pagination.take : undefined,
+    skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
+  });
 
-	return {
-		data: (allowances || []).map((allowance) => ({
-			...allowance,
-			createdAt: allowance.createdAt.toISOString(),
-			updatedAt: allowance.updatedAt.toISOString(),
-		})),
-		count,
-		hasMore,
-	};
+  const hasMore = pagination
+    ? pagination.page * pagination.take < count
+    : false;
+
+  return {
+    data: (allowances || []).map((allowance) => ({
+      ...allowance,
+      createdAt: allowance.createdAt.toISOString(),
+      updatedAt: allowance.updatedAt.toISOString(),
+    })),
+    count,
+    hasMore,
+  };
 };
