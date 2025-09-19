b214174f7ed00204862f0e2f1cbde9280f3f1019
awynx <alwin.puche16@gmail.com>
2025-09-19 18:18:49 +0800

    Log allowance creation and status updates as transactions

--- server/src/models/allowance.ts
+++ server/src/models/allowance.ts
@@ -1,30 +1,67 @@
 import { Allowance, Prisma } from "@prisma/client";
 import { prisma } from "../services/prisma.js";
 import { GetAllowanceArgs } from "../types/index.js";
+import { generateTransactionDescription } from "../services/utils.js";
+import { format } from "date-fns";
 
 export const createAllowance = async (
   data: Omit<
     Allowance,
     "createdAt" | "updatedAt" | "id" | "claimed" | "claimedAt"
   >,
+  systemUserId: string,
 ) => {
-  const allowance = await prisma.allowance.create({
-    data: {
-      year: data.year,
-      month: data.month,
-      totalAmount: data.totalAmount,
-      semester: data.semester,
-      yearLevel: data.yearLevel,
-      bookAllowance: data.bookAllowance || 0,
-      miscellaneousAllowance: data.miscellaneousAllowance || 0,
-      monthlyAllowance: data.monthlyAllowance || 0,
-      thesisAllowance: data.thesisAllowance || 0,
-      student: {
-        connect: {
-          id: data.studentId,
+  const allowance = await prisma.$transaction(async (prsma) => {
+    const user = await prsma.systemUser.findUniqueOrThrow({
+      where: { id: systemUserId },
+    });
+
+    const allwnce = await prsma.allowance.create({
+      data: {
+        year: data.year,
+        month: data.month,
+        totalAmount: data.totalAmount,
+        semester: data.semester,
+        yearLevel: data.yearLevel,
+        bookAllowance: data.bookAllowance || 0,
+        miscellaneousAllowance: data.miscellaneousAllowance || 0,
+        monthlyAllowance: data.monthlyAllowance || 0,
+        thesisAllowance: data.thesisAllowance || 0,
+        student: {
+          connect: {
+            id: data.studentId,
+          },
         },
       },
-    },
+    });
+
+    const student = await prsma.student.findUniqueOrThrow({
+      where: { id: data.studentId },
+    });
+
+    const transaction = await prsma.transaction.create({
+      data: {
+        action: "GENERATE",
+        entity: "ALLOWANCE",
+        entityId: allwnce.id,
+        description: generateTransactionDescription({
+          action: "GENERATE",
+          entity: "ALLOWANCE",
+          performedBy: `${user.firstName} ${user.lastName}`,
+          targetName: `${student.firstName} ${student.lastName}`,
+          extra: `Amount: ${data.totalAmount}, Month/Year: ${format(new Date(new Date().getFullYear(), data.month - 1, 1), "MMMM")}/${data.year}`,
+        }),
+        transactedBy: {
+          connect: {
+            id: systemUserId,
+          },
+        },
+      },
+    });
+
+    if (!transaction) throw new Error("Transaction failed");
+
+    return allwnce;
   });
 
   return {
@@ -34,27 +71,64 @@ export const createAllowance = async (
   };
 };
 
-export const updateAllowanceStatus = async (id: string, claimed: boolean) => {
+export const updateAllowanceStatus = async (
+  id: string,
+  claimed: boolean,
+  transactedById: string,
+) => {
   let claimedAt = null;
 
   if (claimed) {
     claimedAt = new Date();
   }
 
-  const allowance = await prisma.allowance.update({
-    where: {
-      id,
-    },
-    data: {
-      claimed,
-      claimedAt,
-    },
+  const updatedAllowance = await prisma.$transaction(async (tx) => {
+    const user = await tx.systemUser.findUniqueOrThrow({
+      where: { id: transactedById },
+    });
+
+    const allowance = await tx.allowance.update({
+      where: {
+        id,
+      },
+      data: {
+        claimed,
+        claimedAt,
+      },
+      include: { student: true },
+    });
+
+    if (!allowance) throw new Error("Failed to update allowance status!");
+
+    const transaction = await tx.transaction.create({
+      data: {
+        action: "UPDATE",
+        entity: "ALLOWANCE",
+        entityId: allowance.id,
+        description: generateTransactionDescription({
+          action: "UPDATE",
+          entity: "ALLOWANCE",
+          performedBy: `${user.firstName} ${user.lastName}`,
+          targetName: `${allowance.student.firstName} ${allowance.student.lastName}`,
+          extra: `Status: ${claimed ? "Claimed" : "Unclaimed"}`,
+        }),
+        transactedBy: {
+          connect: {
+            id: transactedById,
+          },
+        },
+      },
+    });
+
+    if (!transaction) throw new Error("Transaction failed");
+
+    return allowance;
   });
 
   return {
-    ...allowance,
-    createdAt: allowance.createdAt.toISOString(),
-    updatedAt: allowance.updatedAt.toISOString(),
+    ...updatedAllowance,
+    createdAt: updatedAllowance.createdAt.toISOString(),
+    updatedAt: updatedAllowance.updatedAt.toISOString(),
   };
 };
 
