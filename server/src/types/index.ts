840845f4f88897f6d1424fa25b24b8be050ac670
awynx <alwin.puche16@gmail.com>
2025-09-19 10:53:09 +0800

    Add transaction types and pagination args to types module

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,10 +1,12 @@
 import {
-	Announcement,
-	Document,
-	SystemUser as PSystemUser,
+  Announcement,
+  Document,
+  SystemUser as PSystemUser,
+  TransactionAction,
+  TransactionEntity,
 } from "@prisma/client";
 import { prisma } from "../services/prisma.js";
-import { SystemUser, SystemUserRole } from "./system-user.js";
+import { PaginationArgs, SystemUser, SystemUserRole } from "./system-user.js";
 
 export * from "./system-user.js";
 export * from "./student.js";
@@ -13,49 +15,56 @@ export * from "./token.js";
 export type DocumentInput = Omit<Document, "id" | "createdAt" | "updatedAt">;
 
 export type CreateSystemUserInput = Omit<
-	SystemUser,
-	"id" | "createdAt" | "updatedAt" | "status" | "verifiedAt"
+  SystemUser,
+  "id" | "createdAt" | "updatedAt" | "status" | "verifiedAt"
 >;
 
+export type TransactionPaginationArgs = {
+  pagination?: PaginationArgs<never>["pagination"];
+  entity?: TransactionEntity;
+  action?: TransactionAction;
+  transactedById?: string;
+};
+
 export interface PaginationResult<T> {
-	count: number;
-	data: T[];
-	hasMore: boolean;
+  count: number;
+  data: T[];
+  hasMore: boolean;
 }
 
 export interface GetAllowanceArgs {
-	studentId?: string;
-	year?: number;
-	semester?: number;
-	month?: number;
-	claimed?: boolean;
-	yearLevel?: number;
-	pagination?: {
-		page: number;
-		take: number;
-	};
-	includeStudent?: boolean;
+  studentId?: string;
+  year?: number;
+  semester?: number;
+  month?: number;
+  claimed?: boolean;
+  yearLevel?: number;
+  pagination?: {
+    page: number;
+    take: number;
+  };
+  includeStudent?: boolean;
 }
 
 export interface AnnouncementWithRelation extends Announcement {
-	createdBy: PSystemUser;
+  createdBy: PSystemUser;
 }
 
 export type AppContext = {
-	id: string;
-	email: string;
-	role: SystemUserRole | "STUDENT";
-	prisma: typeof prisma;
+  id: string;
+  email: string;
+  role: SystemUserRole | "STUDENT";
+  prisma: typeof prisma;
 };
 
 export type Gender = "MALE" | "FEMALE";
 
 export interface CalendarEvent {
-	id: string;
-	start: string;
-	end: string;
-	location: string;
-	title: string;
-	backgroundColor: string;
-	borderColor: string;
+  id: string;
+  start: string;
+  end: string;
+  location: string;
+  title: string;
+  backgroundColor: string;
+  borderColor: string;
 }
