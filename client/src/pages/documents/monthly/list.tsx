6a83478ec078220237b8117cdad507a4f7d6d013
awynx <alwin.puche16@gmail.com>
2025-09-25 23:20:27 +0800

    Add late submission request button for past months

--- client/src/pages/documents/monthly/list.tsx
+++ client/src/pages/documents/monthly/list.tsx
@@ -15,231 +15,247 @@ import { PreviewModal, FileTree, DocumentTable } from "@/components";
 import { getFileExtension, imagesExtensions } from "@/lib/constant";
 
 export const generateFolders = (
-	date: string,
-	yearLevelJoined: number
+  date: string,
+  yearLevelJoined: number,
 ): FileTreeItem[] => {
-	const selectedYear = new Date(date).getFullYear();
-	const currentYear = new Date().getFullYear();
-	const items: FileTreeItem[] = [];
+  const selectedYear = new Date(date).getFullYear();
+  const currentYear = new Date().getFullYear();
+  const items: FileTreeItem[] = [];
 
-	// Create the month folders dynamically
-	const months = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	];
+  // Create the month folders dynamically
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-	let plusYear = 0;
+  let plusYear = 0;
 
-	if (yearLevelJoined === 1) {
-		plusYear = 6;
-	} else if (yearLevelJoined === 2) {
-		plusYear = 5;
-	} else if (yearLevelJoined === 3) {
-		plusYear = 4;
-	} else if (yearLevelJoined === 4) {
-		plusYear = 3;
-	} else if (yearLevelJoined === 5) {
-		plusYear = 2;
-	}
+  if (yearLevelJoined === 1) {
+    plusYear = 6;
+  } else if (yearLevelJoined === 2) {
+    plusYear = 5;
+  } else if (yearLevelJoined === 3) {
+    plusYear = 4;
+  } else if (yearLevelJoined === 4) {
+    plusYear = 3;
+  } else if (yearLevelJoined === 5) {
+    plusYear = 2;
+  }
 
-	for (let year = selectedYear; year < selectedYear + plusYear; year++) {
-		items.push({
-			id: year.toString(),
-			name: `${year}`,
-			type: "folder",
-			disabled: year > currentYear,
-			children: months.map((month, index) => ({
-				id: `${year}-${index + 1}`,
-				name: month,
-				type: "file",
-				children: [],
-			})),
-		});
-	}
+  for (let year = selectedYear; year < selectedYear + plusYear; year++) {
+    items.push({
+      id: year.toString(),
+      name: `${year}`,
+      type: "folder",
+      disabled: year > currentYear,
+      children: months.map((month, index) => ({
+        id: `${year}-${index + 1}`,
+        name: month,
+        type: "file",
+        children: [],
+      })),
+    });
+  }
 
-	return items;
+  return items;
 };
 
 export default function Monthly() {
-	const [searchParams] = useSearchParams();
-	const defaultActiveField = searchParams.get("active") || "";
-	const { studentUser } = useAuth();
-	const [activeFileId, setActiveFileId] = useState<string>(defaultActiveField);
-	const currentYear = new Date().getFullYear();
-	const currentMonth = new Date().getMonth() + 1;
-	// const [data, setData] = useState<Document[]>([]);
-	// const [fetchDocuments, { loading, error, refetch }] = useLazyQuery(
-	// 	READ_DOCUMENTS_QUERY,
-	// 	{
-	// 		fetchPolicy: "no-cache",
-	// 	}
-	// );
-	const { data, loading, error, refetch } = useQuery<{ documents: Document[] }>(
-		READ_DOCUMENTS_QUERY,
-		{
-			fetchPolicy: "no-cache",
+  const [searchParams] = useSearchParams();
+  const defaultActiveField = searchParams.get("active") || "";
+  const { studentUser } = useAuth();
+  const [activeFileId, setActiveFileId] = useState<string>(defaultActiveField);
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const activeFieldMonth = Number(activeFileId.split("-")[1]);
+  const activeFieldYear = Number(activeFileId.split("-")[0]);
+  // const [data, setData] = useState<Document[]>([]);
+  // const [fetchDocuments, { loading, error, refetch }] = useLazyQuery(
+  // 	READ_DOCUMENTS_QUERY,
+  // 	{
+  // 		fetchPolicy: "no-cache",
+  // 	}
+  // );
+  const { data, loading, error, refetch } = useQuery<{ documents: Document[] }>(
+    READ_DOCUMENTS_QUERY,
+    {
+      fetchPolicy: "no-cache",
 
-			variables: {
-				month: Number(activeFileId.split("-")[1]),
-				year: Number(activeFileId.split("-")[0]),
-			},
-		}
-	);
-	const [previewModal, onPreviewModalChange] = useState(false);
-	const [toPreview, setToPreview] = useState<string | null>(null);
+      variables: {
+        month: Number(activeFileId.split("-")[1]),
+        year: Number(activeFileId.split("-")[0]),
+      },
+    },
+  );
+  const [previewModal, onPreviewModalChange] = useState(false);
+  const [toPreview, setToPreview] = useState<string | null>(null);
 
-	const handleFileSelect = async (fileId: string) => {
-		setActiveFileId(fileId);
-		// setIsLoading(true);
+  const handleFileSelect = async (fileId: string) => {
+    setActiveFileId(fileId);
+    // setIsLoading(true);
 
-		// const { data } = await fetchDocuments({
-		// 	variables: {
-		// 		month: Number(fileId.split("-")[1]),
-		// 		year: Number(fileId.split("-")[0]),
-		// 	},
-		// });
+    // const { data } = await fetchDocuments({
+    // 	variables: {
+    // 		month: Number(fileId.split("-")[1]),
+    // 		year: Number(fileId.split("-")[0]),
+    // 	},
+    // });
 
-		// setIsLoading(false);
-		// setData(data.documents || []);
-	};
-	const handleRefetch = async () => {
-		await refetch({
-			variables: {
-				month: Number(activeFileId.split("-")[1]),
-				year: Number(activeFileId.split("-")[0]),
-			},
-		});
-	};
+    // setIsLoading(false);
+    // setData(data.documents || []);
+  };
+  const handleRefetch = async () => {
+    await refetch({
+      variables: {
+        month: Number(activeFileId.split("-")[1]),
+        year: Number(activeFileId.split("-")[0]),
+      },
+    });
+  };
 
-	return (
-		<>
-			<Helmet>
-				<meta charSet="utf-8" />
-				<title>Monthly Documents | SEDP</title>
-				<meta
-					name="description"
-					content="View and manage your monthly documents for the SEDP scholarship program."
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Helmet>
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Monthly Documents | SEDP</title>
+        <meta
+          name="description"
+          content="View and manage your monthly documents for the SEDP scholarship program."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
 
-			<div className="px-5 container mx-auto md:px-0">
-				<div className="">
-					<div className="text-2xl font-semibold">Monthly Documents</div>
-					<div className="text-sm text-gray-500">
-						Here you can view and manage your monthly documents.
-					</div>
-				</div>
+      <div className="px-5 container mx-auto md:px-0">
+        <div className="">
+          <div className="text-2xl font-semibold">Monthly Documents</div>
+          <div className="text-sm text-gray-500">
+            Here you can view and manage your monthly documents.
+          </div>
+        </div>
 
-				<div className=" mt-5 max-h-[calc(100dvh-35dvh)]   overflow-y-auto relative md:flex">
-					<div className="md:sticky overflow-auto  relative px-2 md:top-0 md:left-0   md:w-[200px] md:max-w-[200px]">
-						<div className="sticky z-20 py-2 bg-white/5 scroll-py-32 top-0 backdrop-blur-md">
-							<h1>Folders</h1>
-							<p className="text-sm mb-2 text-gray-500">
-								Select a month to view documents.{" "}
-								{formatDate(new Date(`${activeFileId}-01`), "MMM yyyy	")}
-							</p>
-						</div>
-						<FileTree
-							activeFileId={activeFileId}
-							onFileSelect={handleFileSelect}
-							items={generateFolders(
-								studentUser?.createdAt || new Date().toISOString(),
-								studentUser?.yearLevelJoined!
-							)}
-						/>
-					</div>
-					<div className="md:ml-[s210px] relative w-full md:w-[calc(100%-210px)]   mt-5 md:mt-0  overflow-hidden ">
-						{!activeFileId ? (
-							<div className="flex items-center min-h-[calc(100dvh-50dvh)] justify-center w-full h-full">
-								<p className="text-2xl font-semibold text-gray-500">
-									Select a folder.
-								</p>
-							</div>
-						) : error ? (
-							<ErrorFetching handleRefetch={handleRefetch} />
-						) : (
-							<>
-								<div className="absolute p-2 flex max-h-[80px] justify-between md:p-4 top-0 left-0 w-full  bg-primary  bg-opacity-5 backdrop-blur-md   z-10">
-									<h1 className="text-lg  md:text-2xl md:p-2 font-semibold text-gray-500">
-										Documents for{" "}
-										{formatDate(new Date(`${activeFileId}-01`), "MMMM yyyy")}
-									</h1>
-									<div className="flex flex-col items-end md:flex-row gap-2">
-										<Button
-											className={`${loading ? "animate-spin" : ""}`}
-											isDisabled={loading}
-											onPress={handleRefetch}
-											isIconOnly
-											variant="light"
-											radius="full">
-											<Icon
-												className={``}
-												icon="solar:refresh-circle-linear"
-												width="24"
-												height="24"
-											/>
-										</Button>
-										{`${currentYear}-${currentMonth}` === activeFileId && (
-											<Button
-												as={Link}
-												to={`/my-documents/monthly/upload`}
-												color="primary"
-												state={{
-													month: activeFileId.split("-")[1],
-													year: activeFileId.split("-")[0],
-												}}>
-												Upload Document
-											</Button>
-										)}
-									</div>
-								</div>
-								<div className="overflow-y-auto max-h-[calc(100dvh-30dvh)] pb-20 pt-[85px]">
-									{/* <div className="min-h-[300px] bg-red-500 w-full"></div>
+        <div className=" mt-5 max-h-[calc(100dvh-35dvh)]   overflow-y-auto relative md:flex">
+          <div className="md:sticky overflow-auto  relative px-2 md:top-0 md:left-0   md:w-[200px] md:max-w-[200px]">
+            <div className="sticky z-20 py-2 bg-white/5 scroll-py-32 top-0 backdrop-blur-md">
+              <h1>Folders</h1>
+              <p className="text-sm mb-2 text-gray-500">
+                Select a month to view documents.{" "}
+                {formatDate(new Date(`${activeFileId}-01`), "MMM yyyy	")}
+              </p>
+            </div>
+            <FileTree
+              activeFileId={activeFileId}
+              onFileSelect={handleFileSelect}
+              items={generateFolders(
+                studentUser?.createdAt || new Date().toISOString(),
+                studentUser?.yearLevelJoined!,
+              )}
+            />
+          </div>
+          <div className="md:ml-[s210px] relative w-full md:w-[calc(100%-210px)]   mt-5 md:mt-0  overflow-hidden ">
+            {!activeFileId ? (
+              <div className="flex items-center min-h-[calc(100dvh-50dvh)] justify-center w-full h-full">
+                <p className="text-2xl font-semibold text-gray-500">
+                  Select a folder.
+                </p>
+              </div>
+            ) : error ? (
+              <ErrorFetching handleRefetch={handleRefetch} />
+            ) : (
+              <>
+                <div className="absolute p-2 flex max-h-[80px] justify-between md:p-4 top-0 left-0 w-full  bg-primary  bg-opacity-5 backdrop-blur-md   z-10">
+                  <h1 className="text-lg  md:text-2xl md:p-2 font-semibold text-gray-500">
+                    Documents for{" "}
+                    {formatDate(new Date(`${activeFileId}-01`), "MMMM yyyy")}
+                  </h1>
+                  <div className="flex flex-col md:items-center items-end md:flex-row gap-2">
+                    <Button
+                      className={`${loading ? "animate-spin" : ""}`}
+                      isDisabled={loading}
+                      onPress={handleRefetch}
+                      isIconOnly
+                      variant="light"
+                      radius="full"
+                    >
+                      <Icon
+                        className={``}
+                        icon="solar:refresh-circle-linear"
+                        width="24"
+                        height="24"
+                      />
+                    </Button>
+                    {currentYear === activeFieldYear &&
+                      activeFieldMonth < currentMonth && (
+                        <Button
+                          className=""
+                          size="sm"
+                          variant="flat"
+                          color="primary"
+                        >
+                          Request Late Submission
+                        </Button>
+                      )}
+
+                    {`${currentYear}-${currentMonth}` === activeFileId && (
+                      <Button
+                        as={Link}
+                        to={`/my-documents/monthly/upload`}
+                        color="primary"
+                        state={{
+                          month: activeFileId.split("-")[1],
+                          year: activeFileId.split("-")[0],
+                        }}
+                      >
+                        Upload Document
+                      </Button>
+                    )}
+                  </div>
+                </div>
+                <div className="overflow-y-auto max-h-[calc(100dvh-30dvh)] pb-20 pt-[85px]">
+                  {/* <div className="min-h-[300px] bg-red-500 w-full"></div>
 									<div className="min-h-[300px] bg-blue-500 w-full"></div>
 									<div className="min-h-[300px] bg-blue-500 w-full"></div>
 									<div className="min-h-[300px] bg-blue-500 w-full"></div>
 									<div className="min-h-[300px] bg-blue-500 w-full"></div> */}
-									<DocumentTable
-										showAmount
-										handleRowClick={(url) => {
-											onPreviewModalChange(true);
-											setToPreview(url);
-										}}
-										hasActions={
-											`${currentYear}-${currentMonth}` === activeFileId
-										}
-										data={data?.documents || []}
-										isLoading={loading}
-									/>
-								</div>
-							</>
-						)}
-					</div>
-				</div>
-			</div>
-			{toPreview && (
-				<PreviewModal
-					src={toPreview}
-					type={
-						imagesExtensions.includes(getFileExtension(toPreview) || "png")
-							? `image`
-							: "document"
-					}
-					isOpen={previewModal}
-					onOpenChange={onPreviewModalChange}
-				/>
-			)}
-		</>
-	);
+                  <DocumentTable
+                    showAmount
+                    handleRowClick={(url) => {
+                      onPreviewModalChange(true);
+                      setToPreview(url);
+                    }}
+                    hasActions={
+                      `${currentYear}-${currentMonth}` === activeFileId
+                    }
+                    data={data?.documents || []}
+                    isLoading={loading}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      {toPreview && (
+        <PreviewModal
+          src={toPreview}
+          type={
+            imagesExtensions.includes(getFileExtension(toPreview) || "png")
+              ? `image`
+              : "document"
+          }
+          isOpen={previewModal}
+          onOpenChange={onPreviewModalChange}
+        />
+      )}
+    </>
+  );
 }
