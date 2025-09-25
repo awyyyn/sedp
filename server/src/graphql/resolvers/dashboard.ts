494e5cc9b850a0307ddd3aa710b54ae21472a936
awynx <alwin.puche16@gmail.com>
2025-09-25 23:20:37 +0800

    Scope dashboard data by office for non-super admins

--- server/src/graphql/resolvers/dashboard.ts
+++ server/src/graphql/resolvers/dashboard.ts
@@ -1,28 +1,39 @@
 import { GraphQLError } from "graphql";
 import {
-	briefOverviewData,
-	studentsStatusOverview,
+  briefOverviewData,
+  studentsStatusOverview,
 } from "../../models/dashboard.js";
 import { readAnnouncements } from "../../models/announcement.js";
+import { AppContext } from "../../types/index.js";
 
-export const dashboardOverviewDataResolver = async () => {
-	try {
-		const barChartData = await studentsStatusOverview();
-		const briefOverview = await briefOverviewData();
-		const announcements = await readAnnouncements({
-			pagination: {
-				page: 1,
-				take: 3,
-			},
-		});
+export const dashboardOverviewDataResolver = async (
+  _: never,
+  __: never,
+  app: AppContext,
+) => {
+  try {
+    let office: string | undefined = undefined;
 
-		return {
-			chart: barChartData,
-			briefOverview,
-			announcements: announcements.data,
-		};
-	} catch (err) {
-		console.log(err);
-		throw new GraphQLError("Internal Server Error!");
-	}
+    if (app.role !== "SUPER_ADMIN") {
+      office = app.office;
+    }
+
+    const barChartData = await studentsStatusOverview({ office });
+    const briefOverview = await briefOverviewData({ office });
+    const announcements = await readAnnouncements({
+      pagination: {
+        page: 1,
+        take: 3,
+      },
+    });
+
+    return {
+      chart: barChartData,
+      briefOverview,
+      announcements: announcements.data,
+    };
+  } catch (err) {
+    console.log(err);
+    throw new GraphQLError("Internal Server Error!");
+  }
 };
