6e9e2bfb7933a02a3a5cd2d10e963fa0b3a30170
awynx <alwin.puche16@gmail.com>
2025-10-08 14:49:50 +0800

    Revamp admin dashboard with new stats and filters

--- client/src/pages/admin/dashboard/dashboard.tsx
+++ client/src/pages/admin/dashboard/dashboard.tsx
@@ -1,122 +1,317 @@
 import { useQuery } from "@apollo/client";
 import { Tab, Tabs } from "@heroui/tabs";
+import { Helmet } from "react-helmet";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { Select, SelectItem, SelectSection } from "@heroui/select";
+import { useState } from "react";
+import { Divider } from "@heroui/divider";
+import { Card, CardBody } from "@heroui/card";
 
 import MiniInfoCard from "./__components/mini-info-card";
 import { RecentAnnouncements } from "./__components/announcement";
-import { ScholarStatusChart } from "./__components/scholar-status-chart";
+import ScholarStatusStat from "./__components/scholar-status-stat";
+import RecentTransactions from "./__components/recent-transactions";
 
-import { DashboardOverviewData } from "@/types";
-import { READ_DASHBOARD_DATA } from "@/queries";
+import {
+  Announcement,
+  PaginationResult,
+  ReportsByOfficeData,
+  Transaction,
+} from "@/types";
+import { READ_REPORTS_BY_OFFICE_QUERY } from "@/queries";
 import { ErrorUI } from "@/components";
-import { Helmet } from "react-helmet";
+import {
+  headingClasses,
+  officesOptions as opts,
+  schoolOptions,
+} from "@/lib/constant";
+import { formatCurrency } from "@/lib/utils";
+
+const officesOptions = [
+  {
+    province: "All Offices",
+    offices: ["All Offices"],
+  },
+].concat(opts);
 
 export default function Dashboard() {
-	const { data, loading, error, refetch } = useQuery<{
-		data: DashboardOverviewData;
-	}>(READ_DASHBOARD_DATA);
+  const [selectedOffice, setSelectedOffice] = useState("All Offices");
+  const [schoolName, setSchoolName] = useState("");
+  const { data, loading, error, refetch } = useQuery<{
+    reportsByOffice: ReportsByOfficeData;
+    announcements: PaginationResult<Announcement>;
+    transactions: PaginationResult<Transaction>;
+  }>(READ_REPORTS_BY_OFFICE_QUERY, {
+    variables: {
+      office: selectedOffice === "All Offices" ? undefined : selectedOffice,
+      pagination: {
+        page: 1,
+        take: 5,
+      },
+      schoolName: schoolName || undefined,
+      input: {
+        office: selectedOffice === "All Offices" ? undefined : selectedOffice,
+        pagination: {
+          page: 1,
+          take: 10,
+        },
+      },
+    },
+  });
+  // const {
+  //   data: officesData,
+  //   loading: officesLoading,
+  //   error: officesError,
+  //   refetch: refetchOffices,
+  // } = useQuery<{
+  //   officesReports: OfficesReportData[];
+  // }>(READ_OFFICES_REPORTS_QUERY);
 
-	if (loading) {
-		return (
-			<div className="flex items-center justify-center w-full h-full">
-				<Icon
-					icon="lucide:loader-2"
-					className="w-8 h-8 animate-spin text-gray-500"
-				/>
-			</div>
-		);
-	}
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <Icon
+          icon="lucide:loader-2"
+          className="w-8 h-8 animate-spin text-gray-500"
+        />
+      </div>
+    );
+  }
 
-	if (error) {
-		return (
-			<div className="flex h-full items-center justify-center">
-				<ErrorUI onRefresh={refetch} />
-			</div>
-		);
-	}
+  if (error) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <ErrorUI
+          onRefresh={() => {
+            refetch();
+            // refetchOffices();
+          }}
+        />
+      </div>
+    );
+  }
 
-	return (
-		<>
-			<Helmet>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>Dashboard | Admin</title>
-				<meta
-					name="description"
-					content="View the dashboard to get an overview of scholars, events, and announcements."
-				/>
-			</Helmet>
-			<div className=" container  space-y-3">
-				<h1 className="text-3xl font-medium">Dashboard</h1>
-				<Tabs>
-					<Tab title="Overview" className=" ">
-						<div className="space-y-5 ">
-							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-								<MiniInfoCard
-									description={`+${data?.data.briefOverview.totalScholars.avg}% from last month`}
-									icon={
-										<Icon
-											icon="solar:users-group-two-rounded-broken"
-											width="24"
-											height="24"
-											className="text-gray-500"
-										/>
-									}
-									title="Total Students"
-									value={data?.data.briefOverview.totalScholars.new || 0}
-								/>
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Dashboard | Admin</title>
+        <meta
+          name="description"
+          content="View the dashboard to get an overview of scholars, events, and announcements."
+        />
+      </Helmet>
+      <div className=" container  space-y-3 pb-10">
+        <div className="flex md:items-center md:justify-between flex-col md:flex-row gap-2">
+          <h1 className="text-3xl font-medium">Dashboard</h1>
+          <div className="flex gap-2 flex-col md:flex-row">
+            <Autocomplete
+              size="sm"
+              label="School Name"
+              defaultItems={schoolOptions}
+              selectedKey={schoolName}
+              name="schoolName"
+              onSelectionChange={(e) => {
+                setSchoolName(e as string);
+              }}
+              className="lg:min-w-[350px] "
+            >
+              {(item) => (
+                <AutocompleteItem key={item.key}>{item.label}</AutocompleteItem>
+              )}
+            </Autocomplete>
+            <Select
+              disallowEmptySelection
+              className=" border-none ring-0 outline-none lg:min-w-[180px]"
+              size="sm"
+              label="Office"
+              variant="flat"
+              scrollShadowProps={{
+                isEnabled: false,
+              }}
+              selectedKeys={[selectedOffice]}
+              onChange={(e) => setSelectedOffice(e.target.value)}
+            >
+              {officesOptions.map((office) => {
+                return (
+                  <SelectSection
+                    classNames={{
+                      heading: headingClasses,
+                    }}
+                    showDivider
+                    title={office.province}
+                    key={office.province}
+                  >
+                    {office.offices.map((office) => (
+                      <SelectItem key={office}>{office}</SelectItem>
+                    ))}
+                  </SelectSection>
+                );
+              })}
+            </Select>
+          </div>
+        </div>
+        <Divider className="my-3" />
+        <br />
 
-								<MiniInfoCard
-									description={`+${data?.data.briefOverview.activeScholars.avg}% from last semester`}
-									icon={
-										<Icon
-											icon="mdi:user-multiple-check"
-											width="24"
-											height="24"
-											className="text-gray-500"
-										/>
-									}
-									title="Active Scholars"
-									value={data?.data.briefOverview.activeScholars.new || 0}
-								/>
-								<MiniInfoCard
-									description={`${data?.data.briefOverview.graduated.avg}% from total number of scholars`}
-									icon={
-										<Icon
-											icon="simple-icons:googlescholar"
-											width="24"
-											height="24"
-											className="text-gray-500"
-										/>
-									}
-									title="Graduated Scholars"
-									value={data?.data.briefOverview.graduated.new || 0}
-								/>
-								<MiniInfoCard
-									title="Events"
-									value={data?.data.briefOverview.events.new || 0}
-									description={`+${data?.data.briefOverview.events.avg}% from last month`}
-									icon={
-										<Icon
-											icon="mdi:events"
-											width="24"
-											height="24"
-											className="text-gray-500"
-										/>
-									}
-								/>
-							</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <MiniInfoCard
+            description={`${data?.reportsByOffice.totalActiveScholars} active scholars`}
+            icon={
+              <Icon
+                icon="solar:users-group-two-rounded-broken"
+                width="24"
+                height="24"
+                className="text-gray-500"
+              />
+            }
+            title="Total Scholars"
+            value={data?.reportsByOffice.totalScholars || 0}
+          />
+          <MiniInfoCard
+            description={`currently enrolled`}
+            icon={
+              <Icon
+                icon="solar:users-group-two-rounded-broken"
+                width="24"
+                height="24"
+                className="text-gray-500"
+              />
+            }
+            title="Active Scholars"
+            value={data?.reportsByOffice.totalActiveScholars || 0}
+          />
+          <MiniInfoCard
+            description={`Completed program`}
+            icon={
+              <Icon
+                icon="solar:users-group-two-rounded-broken"
+                width="24"
+                height="24"
+                className="text-gray-500"
+              />
+            }
+            title="Graduated Scholars"
+            value={data?.reportsByOffice.totalGraduatesScholars || 0}
+          />
+          <MiniInfoCard
+            description={`Distributed this year`}
+            icon={
+              <Icon
+                icon="tabler:currency-peso"
+                width="24"
+                height="24"
+                className="text-gray-500"
+              />
+            }
+            title="Total Allowances"
+            value={formatCurrency(data?.reportsByOffice.totalAllowance || 0)}
+          />
+        </div>
 
-							<div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
-								<ScholarStatusChart data={data?.data.chart} />
-								<RecentAnnouncements
-									announcements={data?.data.announcements || []}
-								/>
-							</div>
-						</div>
-					</Tab>
-				</Tabs>
-			</div>
-		</>
-	);
+        <br />
+
+        <Tabs aria-label="dashbaord tabs" className="">
+          <Tab key="overview" title="Overview">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 mb-8">
+              <Card className="p-5">
+                <div className=" pb-2">
+                  <h1 className="leading-5 text-lg font-medium text-muted-foreground">
+                    Student Status Distribution
+                  </h1>
+                  <p className="text-gray-400">Breakdown by current status</p>
+                </div>
+                <CardBody className="space-y-5">
+                  <ScholarStatusStat
+                    color="primary"
+                    title="SCHOLAR"
+                    total={data?.reportsByOffice.totalScholars || 0}
+                    value={data?.reportsByOffice.totalActiveScholars || 0}
+                  />
+                  <ScholarStatusStat
+                    color="success"
+                    title="GRADUATED"
+                    total={data?.reportsByOffice.totalScholars || 0}
+                    value={data?.reportsByOffice.totalGraduatesScholars || 0}
+                  />
+                  <ScholarStatusStat
+                    color="warning"
+                    title="DISQUALIFIED"
+                    total={data?.reportsByOffice.totalScholars || 0}
+                    value={data?.reportsByOffice.totalDisqualifiedScholars || 0}
+                  />
+                </CardBody>
+              </Card>
+              <Card className="p-5">
+                <div className="    pb-2">
+                  <h1 className="leading-5 text-lg font-medium text-muted-foreground">
+                    Allowance Distribution
+                  </h1>
+                  <p className="text-gray-400">Breakdown by allowance type</p>
+                </div>
+                <CardBody className="space-y-5">
+                  <ScholarStatusStat
+                    isAllowance
+                    color="primary"
+                    title="Monthly Allowance"
+                    total={data?.reportsByOffice.totalAllowance || 0}
+                    value={data?.reportsByOffice.totalMonthlyAllowance || 0}
+                  />
+                  <ScholarStatusStat
+                    isAllowance
+                    color="success"
+                    title="Book Allowance"
+                    total={data?.reportsByOffice.totalAllowance || 0}
+                    value={data?.reportsByOffice.totalBookAllowance || 0}
+                  />
+                  <ScholarStatusStat
+                    color="secondary"
+                    title="Thesis Allowance"
+                    isAllowance
+                    total={data?.reportsByOffice.totalAllowance || 0}
+                    value={data?.reportsByOffice.totalThesisAllowance || 0}
+                  />
+                  <ScholarStatusStat
+                    color="warning"
+                    title="Miscellaneous"
+                    isAllowance
+                    total={data?.reportsByOffice.totalAllowance || 0}
+                    value={
+                      data?.reportsByOffice.totalMiscellaneousAllowance || 0
+                    }
+                  />
+
+                  <Divider />
+                  <div className="flex justify-between items-center">
+                    <h1 className="font-semibold text-lg">Total</h1>
+                    <h1 className="font-semibold text-lg">
+                      {formatCurrency(
+                        data?.reportsByOffice.totalAllowance || 0,
+                      )}
+                    </h1>
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+
+            <div>
+              <div>
+                <RecentAnnouncements
+                  announcements={data?.announcements.data || []}
+                />
+              </div>
+              <div>
+                <UpcomingEvents events={[]} />
+              </div>
+            </div>
+          </Tab>
+
+          <Tab key="transactions" title="Transactions">
+            <RecentTransactions transactions={data?.transactions.data || []} />
+          </Tab>
+        </Tabs>
+      </div>
+    </>
+  );
 }
