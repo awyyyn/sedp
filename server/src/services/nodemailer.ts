c636615097d9b6bf9d42838580ee2a597f850345
awynx <alwin.puche16@gmail.com>
2025-10-14 11:55:24 +0800

    Use service for Gmail transporter

--- server/src/services/nodemailer.ts
+++ server/src/services/nodemailer.ts
@@ -2,7 +2,7 @@ import { environment } from "../environments/environment.js";
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "gmail",
+  service: "gmail",
   auth: {
     user: environment.EMAIL,
     pass: environment.PASSWORD,
