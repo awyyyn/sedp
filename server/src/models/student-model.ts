5e8e2604535c5bae23e62e3b6639652ad77534b0
awynx <alwin.puche16@gmail.com>
2025-10-14 12:08:54 +0800

    Move mail sending out of DB transactions

--- server/src/models/student-model.ts
+++ server/src/models/student-model.ts
@@ -3,250 +3,253 @@ import { environment } from "../environments/environment.js";
 import { prisma } from "../services/prisma.js";
 import { generateTransactionDescription } from "../services/utils.js";
 import {
-	CreateScholarInput,
-	PaginationArgs,
-	PaginationResult,
-	Student,
-	StudentStatus,
+  CreateScholarInput,
+  PaginationArgs,
+  PaginationResult,
+  Student,
+  StudentStatus,
 } from "../types/index.js";
 import { Prisma } from "@prisma/client";
 import { genSalt, hash } from "bcrypt";
 import { sendCredentials } from "./email-model.js";
 
 export const createStudent = async (
-	values: CreateScholarInput,
-	systemUserId?: string
+  values: CreateScholarInput,
+  systemUserId?: string,
 ): Promise<Student> => {
-	return await prisma.$transaction(async (tx) => {
-		const {
-			address,
-			birthDate,
-			email,
-			firstName,
-			lastName,
-			gender,
-			mfaSecret,
-			middleName,
-			password,
-			course,
-			schoolName,
-			phoneNumber,
-			office,
-			semester,
-			yearLevel,
-		} = values;
+  const student = await prisma.$transaction(async (tx) => {
+    const {
+      address,
+      birthDate,
+      email,
+      firstName,
+      lastName,
+      gender,
+      mfaSecret,
+      middleName,
+      password,
+      course,
+      schoolName,
+      phoneNumber,
+      office,
+      semester,
+      yearLevel,
+    } = values;
 
-		const isStudentExist = await tx.student.count({
-			where: { email: email },
-		});
+    const isStudentExist = await tx.student.count({
+      where: { email: email },
+    });
 
-		const systemUser = await tx.systemUser.findUnique({
-			where: { id: systemUserId },
-		});
+    const systemUser = await tx.systemUser.findUnique({
+      where: { id: systemUserId },
+    });
 
-		if (systemUserId && !systemUser) throw new Error("Unauthorized user");
+    if (systemUserId && !systemUser) throw new Error("Unauthorized user");
 
-		if (isStudentExist)
-			throw new Error(`Student with email ${values.email} already exists.`);
+    if (isStudentExist)
+      throw new Error(`Student with email ${values.email} already exists.`);
 
-		const generateSalt = await genSalt(environment.SALT);
-		const hashedPassword = await hash(password, generateSalt);
+    const generateSalt = await genSalt(environment.SALT);
+    const hashedPassword = await hash(password, generateSalt);
 
-		const newStudentUser = await tx.student.create({
-			data: {
-				password: hashedPassword,
-				office,
-				address,
-				birthDate: new Date(birthDate).toISOString(),
-				email,
-				firstName,
-				course,
-				semester,
-				lastName,
-				phoneNumber,
-				schoolName,
-				yearLevelJoined: yearLevel,
-				gender,
-				yearLevel,
-				middleName,
-				status: "SCHOLAR",
-				mfaEnabled: !!mfaSecret,
-				mfaSecret,
-			},
-		});
+    const newStudentUser = await tx.student.create({
+      data: {
+        password: hashedPassword,
+        office,
+        address,
+        birthDate: new Date(birthDate).toISOString(),
+        email,
+        firstName,
+        course,
+        semester,
+        lastName,
+        phoneNumber,
+        schoolName,
+        yearLevelJoined: yearLevel,
+        gender,
+        yearLevel,
+        middleName,
+        status: "SCHOLAR",
+        mfaEnabled: !!mfaSecret,
+        mfaSecret,
+      },
+    });
 
-		if (!newStudentUser) throw new Error("Error creating scholar user");
+    if (!newStudentUser) throw new Error("Error creating scholar user");
 
-		const emailResult = await sendCredentials({
-			email: newStudentUser.email,
-			password: values.password,
-		});
+    if (systemUser) {
+      const transaction = await tx.transaction.create({
+        data: {
+          action: "CREATE",
+          entity: "STUDENT",
+          description: generateTransactionDescription(
+            "CREATE",
+            "STUDENT",
+            systemUser,
+          ),
+          entityId: newStudentUser.id,
+          transactedBy: {
+            connect: { id: systemUser.id },
+          },
+        },
+      });
 
-		if (emailResult.rejected.length > 0) {
-			throw new Error("There was an error creating account");
-		}
+      if (!transaction) throw new Error("Error creating transaction");
+    }
+    return {
+      ...newStudentUser,
+      birthDate: newStudentUser.birthDate.toISOString(),
+    };
+  });
 
-		if (systemUser) {
-			const transaction = await tx.transaction.create({
-				data: {
-					action: "CREATE",
-					entity: "STUDENT",
-					description: generateTransactionDescription(
-						"CREATE",
-						"STUDENT",
-						systemUser
-					),
-					entityId: newStudentUser.id,
-					transactedBy: {
-						connect: { id: systemUser.id },
-					},
-				},
-			});
+  if (!student) throw new Error("Failed to create student");
+  const emailResult = await sendCredentials({
+    email: student.email,
+    password: values.password,
+  });
 
-			if (!transaction) throw new Error("Error creating transaction");
-		}
-		return {
-			...newStudentUser,
-			birthDate: newStudentUser.birthDate.toISOString(),
-		};
-	});
+  if (emailResult.rejected.length > 0) {
+    throw new Error("There was an error creating account");
+  }
+
+  return student;
 };
 
 export const updateStudent = async (
-	id: string,
-	values: Partial<Student>
+  id: string,
+  values: Partial<Student>,
 ): Promise<Student> => {
-	let toUpdateData = values;
-	let hashedPassword: string | undefined = undefined;
+  let toUpdateData = values;
+  let hashedPassword: string | undefined = undefined;
 
-	if (values.status) {
-		toUpdateData.statusUpdatedAt = new Date();
-	}
+  if (values.status) {
+    toUpdateData.statusUpdatedAt = new Date();
+  }
 
-	if (values.password && values.password.trim()) {
-		const generateSalt = await genSalt(environment.SALT);
-		hashedPassword = await hash(values.password.trim(), generateSalt);
-	}
+  if (values.password && values.password.trim()) {
+    const generateSalt = await genSalt(environment.SALT);
+    hashedPassword = await hash(values.password.trim(), generateSalt);
+  }
 
-	const updatedStudent = await prisma.student.update({
-		data: {
-			...toUpdateData,
-			password: hashedPassword,
-		},
-		where: {
-			id,
-		},
-	});
+  const updatedStudent = await prisma.student.update({
+    data: {
+      ...toUpdateData,
+      password: hashedPassword,
+    },
+    where: {
+      id,
+    },
+  });
 
-	return {
-		...updatedStudent,
-		birthDate: updatedStudent.birthDate.toISOString(),
-	};
+  return {
+    ...updatedStudent,
+    birthDate: updatedStudent.birthDate.toISOString(),
+  };
 };
 
 export const readStudent = async (id: string): Promise<Student | null> => {
-	let where: Prisma.StudentWhereInput = {
-		id: id,
-	};
+  let where: Prisma.StudentWhereInput = {
+    id: id,
+  };
 
-	if (id.includes("@")) {
-		where = {
-			email: id,
-		};
-	}
+  if (id.includes("@")) {
+    where = {
+      email: id,
+    };
+  }
 
-	const student = await prisma.student.findFirst({
-		where,
-	});
+  const student = await prisma.student.findFirst({
+    where,
+  });
 
-	if (!student) return null;
+  if (!student) return null;
 
-	return {
-		...student,
-		birthDate: student.birthDate.toISOString(),
-	};
+  return {
+    ...student,
+    birthDate: student.birthDate.toISOString(),
+  };
 };
 
 export async function readAllStudents({
-	filter,
-	pagination,
-	status,
-	includeDocs,
-	office,
-	school,
+  filter,
+  pagination,
+  status,
+  includeDocs,
+  office,
+  school,
 }: PaginationArgs<StudentStatus> & {
-	includeDocs?: boolean;
-	school?: string;
+  includeDocs?: boolean;
+  school?: string;
 } = {}): Promise<PaginationResult<Student>> {
-	let where: Prisma.StudentWhereInput = {};
+  let where: Prisma.StudentWhereInput = {};
 
-	if (filter) {
-		where = {
-			OR: [
-				{ email: { contains: filter } },
-				{ firstName: { contains: filter } },
-				{ lastName: { contains: filter } },
-			],
-		};
-	}
+  if (filter) {
+    where = {
+      OR: [
+        { email: { contains: filter } },
+        { firstName: { contains: filter } },
+        { lastName: { contains: filter } },
+      ],
+    };
+  }
 
-	if (school?.trim()) {
-		where.schoolName = school.trim();
-	}
+  if (school?.trim()) {
+    where.schoolName = school.trim();
+  }
 
-	if (office) {
-		where.office = office;
-	}
+  if (office) {
+    where.office = office;
+  }
 
-	if (typeof status !== "undefined") {
-		where.status = status;
-	}
+  if (typeof status !== "undefined") {
+    where.status = status;
+  }
 
-	const users = await prisma.student.findMany({
-		where,
-		skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
-		take: pagination ? pagination.take : undefined,
-		include: {
-			documents: includeDocs,
-		},
-	});
+  const users = await prisma.student.findMany({
+    where,
+    skip: pagination ? (pagination.page - 1) * pagination.take : undefined,
+    take: pagination ? pagination.take : undefined,
+    include: {
+      documents: includeDocs,
+    },
+  });
 
-	const count = await prisma.student.count({
-		where,
-	});
+  const count = await prisma.student.count({
+    where,
+  });
 
-	return {
-		data: users.map((user) => ({
-			...user,
-			birthDate: user.birthDate.toISOString(),
-			documents: (user.documents || [])
-				.filter(
-					(doc) =>
-						doc.createdAt.getFullYear() === new Date().getFullYear() &&
-						doc.createdAt.getMonth() === new Date().getMonth() &&
-						doc.monthlyDocument
-				)
-				.map((doc) => ({
-					...doc,
-					createdAt: doc.createdAt.toISOString(),
-					updatedAt: doc.updatedAt.toISOString(),
-				})),
-		})),
-		hasMore: pagination ? pagination.page * pagination.take < count : false,
-		count,
-	};
+  return {
+    data: users.map((user) => ({
+      ...user,
+      birthDate: user.birthDate.toISOString(),
+      documents: (user.documents || [])
+        .filter(
+          (doc) =>
+            doc.createdAt.getFullYear() === new Date().getFullYear() &&
+            doc.createdAt.getMonth() === new Date().getMonth() &&
+            doc.monthlyDocument,
+        )
+        .map((doc) => ({
+          ...doc,
+          createdAt: doc.createdAt.toISOString(),
+          updatedAt: doc.updatedAt.toISOString(),
+        })),
+    })),
+    hasMore: pagination ? pagination.page * pagination.take < count : false,
+    count,
+  };
 }
 
 export const changePassword = async (newPassword: string, id: string) => {
-	const generateSalt = await genSalt(environment.SALT);
-	const hashedPassword = await hash(newPassword, generateSalt);
+  const generateSalt = await genSalt(environment.SALT);
+  const hashedPassword = await hash(newPassword, generateSalt);
 
-	await prisma.student.update({
-		data: {
-			password: hashedPassword,
-		},
-		where: {
-			id,
-		},
-	});
+  await prisma.student.update({
+    data: {
+      password: hashedPassword,
+    },
+    where: {
+      id,
+    },
+  });
 };
