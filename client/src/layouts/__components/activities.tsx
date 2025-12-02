23f164b83345b8e578241a9263d9a5a0ce3f666c
awynx <alwin.puche16@gmail.com>
2025-12-02 20:09:48 +0800

    Add ActivitiesLayout component

---
+++ client/src/layouts/__components/activities.tsx
@@ -0,0 +1,50 @@
+import { Button, ButtonGroup } from "@heroui/button";
+import { cn } from "@heroui/theme";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+export default function ActivitiesLayout() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {pathname !== "/admin/activities" && (
+        <ButtonGroup>
+          <Button
+            onPress={() => navigate("/admin/activities/events")}
+            variant={pathname.includes("events") ? "solid" : "flat"}
+            color={pathname.includes("events") ? "primary" : "default"}
+          >
+            Events
+          </Button>
+          <Button
+            onPress={() => navigate("/admin/activities/meetings")}
+            variant={pathname.includes("meetings") ? "solid" : "flat"}
+            color={pathname.includes("meetings") ? "primary" : "default"}
+          >
+            Meetings
+          </Button>
+          <Button
+            onPress={() => navigate("/admin/activities/announcements")}
+            variant={pathname.includes("announcements") ? "solid" : "flat"}
+            color={pathname.includes("announcements") ? "primary" : "default"}
+            // className={cn(
+            //   // Base classes, if any, can go here first
+
+            //   // Use object syntax for conditional classes:
+            //   {
+            //     "bg-primary text-white": pathname.includes("announcements"),
+            //     "": !pathname.includes("announcements"),
+            //   },
+            // )}
+          >
+            Announcements
+          </Button>
+        </ButtonGroup>
+      )}
+      <section className="mt-5">
+        <Outlet />
+      </section>
+    </>
+  );
+}
