95b01851fec4b94b678827cff6bb49d901a18bf8
awynx <alwin.puche16@gmail.com>
2025-12-02 20:13:54 +0800

    Add edit/delete actions to document table

--- client/src/components/document-table.tsx
+++ client/src/components/document-table.tsx
@@ -1,11 +1,12 @@
 import {
-	Table,
-	TableHeader,
-	TableColumn,
-	TableBody,
-	TableRow,
-	TableCell,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
 } from "@heroui/table";
+import { Link } from "react-router-dom";
 import { Spinner } from "@heroui/spinner";
 import { Button } from "@heroui/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
@@ -18,196 +19,206 @@ import { getFileExtension } from "@/lib/constant";
 import { formatCurrency } from "@/lib/utils";
 import { DeleteModal } from "@/pages/admin/__components";
 import { DELETE_DOCUMENT_MUTATION } from "@/queries";
-import { Link } from "react-router-dom";
 
 export function DocumentTable({
-	data,
-	isLoading = false,
-	handleRowClick,
-	hasActions = false,
-	showAmount = false,
+  data,
+  isLoading = false,
+  handleRowClick,
+  hasActions = false,
+  showAmount = false,
 }: {
-	data: Document[];
-	showAmount?: boolean;
-	isLoading?: boolean;
-	handleRowClick: (url: string) => void;
-	hasActions?: boolean;
+  data: Document[];
+  showAmount?: boolean;
+  isLoading?: boolean;
+  handleRowClick: (url: string) => void;
+  hasActions?: boolean;
 }) {
-	const [deleteModal, setDeleteModal] = useState(false);
-	const [toDelete, setToDelete] = useState<Document | null>(null);
-	const [deleteDocument, { loading: deletingDocument }] = useMutation(
-		DELETE_DOCUMENT_MUTATION
-	);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [toDelete, setToDelete] = useState<Document | null>(null);
+  const [deleteDocument, { loading: deletingDocument }] = useMutation(
+    DELETE_DOCUMENT_MUTATION,
+  );
 
-	if (hasActions) {
-		const handleDelete = async () => {
-			try {
-				if (!toDelete) return;
-				await deleteDocument({
-					variables: {
-						id: toDelete.id,
-					},
-				});
-				toast.success("Document deleted successfully", {
-					description: "Document deleted successfully",
-					position: "top-center",
-					richColors: true,
-				});
-			} catch (err) {
-				toast.error("Something went wrong!", {
-					description: "Error deleting document",
-					position: "top-center",
-					richColors: true,
-				});
-			}
-		};
+  if (hasActions) {
+    const handleDelete = async () => {
+      try {
+        if (!toDelete) return;
+        await deleteDocument({
+          variables: {
+            id: toDelete.id,
+          },
+        });
+        toast.success("Document deleted successfully", {
+          description: "Document deleted successfully",
+          position: "top-center",
+          richColors: true,
+        });
+      } catch (err) {
+        toast.error("Something went wrong!", {
+          description: "Error deleting document",
+          position: "top-center",
+          richColors: true,
+        });
+      }
+    };
 
-		return (
-			<>
-				<Table
-					removeWrapper
-					className="pt-2"
-					aria-label="Documents table"
-					selectionMode="single">
-					<TableHeader>
-						<TableColumn>Name</TableColumn>
-						<TableColumn>File Type</TableColumn>
-						<TableColumn>Type</TableColumn>
-						<TableColumn className={`${!showAmount && "hidden"}`}>
-							Amount
-						</TableColumn>
-						<TableColumn className="text-center">Action</TableColumn>
-					</TableHeader>
-					<TableBody
-						emptyContent="No documents found"
-						loadingContent={<Spinner label="Loading..." />}
-						isLoading={isLoading}>
-						{data.map((doc) => {
-							const exe = getFileExtension(doc.documentUrl);
+    return (
+      <>
+        <Table
+          removeWrapper
+          className="pt-2"
+          aria-label="Documents table"
+          selectionMode="single"
+        >
+          <TableHeader>
+            {/*<TableColumn>Name</TableColumn>*/}
+            <TableColumn>File Type</TableColumn>
+            <TableColumn>Type</TableColumn>
+            <TableColumn className={`${!showAmount && "hidden"}`}>
+              Amount
+            </TableColumn>
+            <TableColumn className="text-center">Action</TableColumn>
+          </TableHeader>
+          <TableBody
+            emptyContent="No documents found"
+            loadingContent={<Spinner label="Loading..." />}
+            isLoading={isLoading}
+          >
+            {data.map((doc) => {
+              const exe = getFileExtension(doc.documentUrl);
 
-							return (
-								<TableRow
-									onClick={() => {
-										if (deletingDocument) return;
-										handleRowClick(doc.documentUrl);
-									}}
-									className="cursor-pointer"
-									key={doc.id}>
-									<TableCell className="max-w-[120px] min-w-[120px]   ">
-										<p className="truncate">{doc.documentName}</p>
-									</TableCell>
-									<TableCell>{exe?.toUpperCase()}</TableCell>
-									<TableCell className="capitalize">
-										{(doc.docType === "OTHER"
-											? doc.otherType
-											: doc.docType
-										)?.toLowerCase()}
-									</TableCell>
-									<TableCell className={`${!showAmount && "hidden"}`}>
-										{formatCurrency(doc.amount)}
-									</TableCell>
+              return (
+                <TableRow
+                  onClick={() => {
+                    if (deletingDocument) return;
+                    handleRowClick(doc.documentUrl);
+                  }}
+                  className="cursor-pointer"
+                  key={doc.id}
+                >
+                  {/*<TableCell className="max-w-[120px] min-w-[120px]   ">
+                    <p className="truncate">
+                      {doc.documentName.replace(/_/g, " ")}
+                    </p>
+                  </TableCell>*/}
+                  <TableCell>{exe?.toUpperCase()}</TableCell>
+                  <TableCell className="capitalize">
+                    {(doc.docType === "OTHER" ? doc.otherType : doc.docType)
+                      ?.toLowerCase()
+                      .replace(/_/g, " ")}
+                  </TableCell>
+                  <TableCell className={`${!showAmount && "hidden"}`}>
+                    {doc.docType !== "RECEIPT"
+                      ? "N/A"
+                      : formatCurrency(doc.amount)}
+                  </TableCell>
 
-									<TableCell className="flex items-center gap-2 justify-center">
-										<Button
-											isIconOnly
-											size="sm"
-											as={Link}
-											to={`/my-documents/monthly/${doc.id}/edit`}
-											state={{
-												document: doc,
-												month: doc.month,
-												year: doc.year,
-											}}
-											isDisabled={deletingDocument}
-											// variant="bordered"
-											color="primary">
-											<Icon
-												icon="hugeicons:pencil-edit-02"
-												width="16"
-												height="16"
-											/>
-										</Button>
-										<Button
-											isIconOnly
-											isDisabled={deletingDocument}
-											size="sm"
-											onPress={() => {
-												setToDelete(doc);
-												setDeleteModal(true);
-											}}
-											// variant="bordered"
-											color="danger">
-											<Icon icon="hugeicons:delete-01" width="16" height="16" />
-										</Button>
-									</TableCell>
-								</TableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-				{toDelete && (
-					<DeleteModal
-						handleDeletion={handleDelete}
-						loading={false}
-						open={deleteModal}
-						setOpen={setDeleteModal}
-						title="Delete Document"
-						deleteLabel="Delete Document"
-						description="Are you sure you want to delete this document? This action cannot be undone."
-					/>
-				)}
-			</>
-		);
-	}
+                  <TableCell className="flex items-center gap-2 justify-center">
+                    <Button
+                      isIconOnly
+                      size="sm"
+                      as={Link}
+                      to={`/my-documents/monthly/${doc.id}/edit`}
+                      state={{
+                        document: doc,
+                        month: doc.month,
+                        year: doc.year,
+                      }}
+                      isDisabled={deletingDocument}
+                      // variant="bordered"
+                      color="primary"
+                    >
+                      <Icon
+                        icon="hugeicons:pencil-edit-02"
+                        width="16"
+                        height="16"
+                      />
+                    </Button>
+                    <Button
+                      isIconOnly
+                      isDisabled={deletingDocument}
+                      size="sm"
+                      onPress={() => {
+                        setToDelete(doc);
+                        setDeleteModal(true);
+                      }}
+                      // variant="bordered"
+                      color="danger"
+                    >
+                      <Icon icon="hugeicons:delete-01" width="16" height="16" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        {toDelete && (
+          <DeleteModal
+            handleDeletion={handleDelete}
+            loading={false}
+            open={deleteModal}
+            setOpen={setDeleteModal}
+            title="Delete Document"
+            deleteLabel="Delete Document"
+            description="Are you sure you want to delete this document? This action cannot be undone."
+          />
+        )}
+      </>
+    );
+  }
 
-	return (
-		<>
-			<Table
-				removeWrapper
-				className="pt-2"
-				aria-label="Documents table"
-				selectionMode="single">
-				<TableHeader>
-					<TableColumn>Name</TableColumn>
-					<TableColumn>File Type</TableColumn>
-					<TableColumn>Type</TableColumn>
-					<TableColumn className={`${!showAmount && "hidden"}`}>
-						Amount
-					</TableColumn>
-				</TableHeader>
-				<TableBody
-					emptyContent="No documents found"
-					loadingContent={<Spinner label="Loading..." />}
-					isLoading={isLoading}>
-					{data.map((doc) => {
-						const exe = getFileExtension(doc.documentUrl);
-						const isCor = doc.docType === "COR";
-						const docLabel = isCor ? "COR/COE" : doc.documentName;
-						const docType =
-							doc.docType === "OTHER"
-								? doc.otherType
-								: isCor
-									? "COR/COE"
-									: doc.docType;
+  return (
+    <>
+      <Table
+        removeWrapper
+        className="pt-2"
+        aria-label="Documents table"
+        selectionMode="single"
+      >
+        <TableHeader>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>File Type</TableColumn>
+          <TableColumn>Type</TableColumn>
+          <TableColumn className={`${!showAmount && "hidden"}`}>
+            Amount
+          </TableColumn>
+        </TableHeader>
+        <TableBody
+          emptyContent="No documents found"
+          loadingContent={<Spinner label="Loading..." />}
+          isLoading={isLoading}
+        >
+          {data.map((doc) => {
+            const exe = getFileExtension(doc.documentUrl);
+            const isCor = doc.docType === "COR";
+            const docLabel = isCor ? "COR/COE" : doc.documentName;
+            const docType =
+              doc.docType === "OTHER"
+                ? doc.otherType
+                : isCor
+                  ? "COR/COE"
+                  : doc.docType;
 
-						return (
-							<TableRow
-								onClick={() => handleRowClick(doc.documentUrl)}
-								className="cursor-pointer"
-								key={doc.id}>
-								<TableCell>{docLabel.replace("_", " ")}</TableCell>
-								<TableCell>{exe?.toUpperCase()}</TableCell>
-								<TableCell className="capitalize">
-									{docType?.replace(/_/g, " ")}
-								</TableCell>
-								<TableCell className={`${!showAmount && "hidden"}`}>
-									{formatCurrency(doc.amount)}
-								</TableCell>
-							</TableRow>
-						);
-					})}
-				</TableBody>
-			</Table>
-		</>
-	);
+            return (
+              <TableRow
+                onClick={() => handleRowClick(doc.documentUrl)}
+                className="cursor-pointer"
+                key={doc.id}
+              >
+                <TableCell>{docLabel.replace("_", " ")}</TableCell>
+                <TableCell>{exe?.toUpperCase()}</TableCell>
+                <TableCell className="capitalize">
+                  {docType?.replace(/_/g, " ")}
+                </TableCell>
+                <TableCell className={`${!showAmount && "hidden"}`}>
+                  {formatCurrency(doc.amount)}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
