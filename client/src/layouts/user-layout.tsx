33e5d4491f8a4c8d77a2d016d1adc525e99eed79
awynx <alwin.puche16@gmail.com>
2025-12-04 10:09:26 +0800

    Normalize indentation in UserLayout

--- client/src/layouts/user-layout.tsx
+++ client/src/layouts/user-layout.tsx
@@ -3,12 +3,12 @@ import { Outlet } from "react-router-dom";
 import UserHeader from "./__components/user-header";
 
 export default function UserLayout() {
-	return (
-		<div>
-			<UserHeader />
-			<main className=" ">
-				<Outlet />
-			</main>
-		</div>
-	);
+  return (
+    <div className=" ">
+      <UserHeader />
+      <main className=" ">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
